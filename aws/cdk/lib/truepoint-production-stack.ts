239a8fd9e06ace4c2f747cf83076c99e1f092006
iamsupermazinga <iamsupermazinga@gmail.com>
2020-08-19 23:09:13 +0900

    cdk rds 작성 중. option property 더 자세히 정의 필요

--- aws/cdk/lib/truepoint-production-stack.ts
+++ aws/cdk/lib/truepoint-production-stack.ts
@@ -1,12 +1,13 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as ecs from '@aws-cdk/aws-ecs';
-import * as iam from '@aws-cdk/aws-iam';
 import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
 import * as route53 from '@aws-cdk/aws-route53'
-import * as targets from '@aws-cdk/aws-route53-targets'
-import * as acm from '@aws-cdk/aws-certificatemanager';
+import * as route53targets from '@aws-cdk/aws-route53-targets'
 import * as logs from '@aws-cdk/aws-logs';
+import * as iam from '@aws-cdk/aws-iam';
+import * as acm from '@aws-cdk/aws-certificatemanager';
+import * as rds from '@aws-cdk/aws-rds';
 
 import getSSMParams from '../utils/getParams';
 
@@ -171,7 +172,7 @@ export class TruepointStack extends cdk.Stack {
     // *********************************************
     // *********** Certificate manager *************
     // *********************************************
-    // const sslcert = acm.Certificate.fromCertificateArn(
+    // const sslCert = acm.Certificate.fromCertificateArn(
     //   this, `${ID_PREFIX}DnsCertificates`,
     //   process.env.AWS_SSL_CERTIFICATIE_ARN // 생성 필요.
     // )
@@ -204,15 +205,92 @@ export class TruepointStack extends cdk.Stack {
       }
     });
 
+    // Create Load Balancer
+    const publicLoadBalancer = new elbv2.ApplicationLoadBalancer(this, `${ID_PREFIX}PublicLoadBalancer`, {
+      vpc: vpc,
+      internetFacing: true,
+      loadBalancerName: `${ID_PREFIX}Public`,
+      securityGroup: loadBalancerSecGrp
+    });
+
+    // Define HTTP Listener
+    const HTTPListener = publicLoadBalancer.addListener(`${ID_PREFIX}HttpListener`, {
+      port: 80,
+      defaultTargetGroups: [frontendTargetGroup],
+    });
+    HTTPListener.addRedirectResponse(`${ID_PREFIX}80to443RedirectTarget`, {
+      priority: 1,
+      pathPattern: '/*',
+      statusCode: 'HTTP_301',
+      port: '443',
+      protocol: elbv2.Protocol.HTTPS
+    });
+
+    // Define HTTPS Listener
+    const HTTPSListener = publicLoadBalancer.addListener(`${ID_PREFIX}HttpsListener`, {
+      port: 443,
+      // certificates: [sslCert],
+      sslPolicy: elbv2.SslPolicy.RECOMMENDED,
+      defaultTargetGroups: [frontendTargetGroup],
+    });
+    HTTPSListener.addTargetGroups(`${ID_PREFIX}BackendTargetGroups`, {
+      priority: 1,
+      targetGroups: [backendTargetGroup],
+      hostHeader: BACKEND_DOMAIN,
+    });
+    HTTPSListener.addTargetGroups(`${ID_PREFIX}FrontendTargetGroups`, {
+      priority: 2,
+      targetGroups: [frontendTargetGroup],
+      hostHeader: FRONTEND_DOMAIN
+    });
+  
 
     // *********************************************
     // ***************** Route53 *******************
     // *********************************************
+    const HostedZone = new route53.PublicHostedZone(this, `${ID_PREFIX}HostedZone`, {
+      zoneName: DOMAIN_NAME,
+    });
 
+    new route53.ARecord(this, `${ID_PREFIX}FrontendARecord`, {
+      // recordName = hostedzone root domain name
+      zone: HostedZone,
+      target: route53.RecordTarget.fromAlias(
+        new route53targets.LoadBalancerTarget(publicLoadBalancer)
+      ),
+    });
+
+    new route53.ARecord(this, `${ID_PREFIX}BackendARecord`, {
+      zone: HostedZone,
+      recordName: BACKEND_SUBDOMAIN,
+      target: route53.RecordTarget.fromAlias(
+        new route53targets.LoadBalancerTarget(publicLoadBalancer)
+      )
+    })
     
     // *********************************************
     // ******************* RDS *********************
     // *********************************************
-    
+    const DBInstace = new rds.DatabaseInstance(this, `${ID_PREFIX}ProductionDBInstance`, {
+      vpc: vpc,
+      engine: rds.DatabaseInstanceEngine.mysql({
+        version: rds.MysqlEngineVersion.VER_8_0_17
+      }),
+      masterUsername: ssmParameters.TRUEPOINT_DB_USER.stringValue,
+      // *********************************************
+      // Free tier instance type for testing and developing
+      // please change other instace type when you deploy production
+      // *********************************************
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      // *********************************************
+      // For develop and testing.
+      // You should change this in production deployment
+      // *********************************************
+      multiAz: false, 
+      // Enable storage auto scailing option by specifying maximum allocated storage
+      maxAllocatedStorage: 300, // GB
+
+      // and another option props...
+    })
   }
 }
