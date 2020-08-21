175c26af5a5ed31387187fea173f48ad91f5e6ec
hwasurr <iamsupermazinga@gmail.com>
2020-08-21 18:39:09 +0900

    cdk RDS 설정 변경한 스택

--- aws/cdk/bin/cdk.ts
+++ aws/cdk/bin/cdk.ts
@@ -3,13 +3,43 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import * as dotenv from 'dotenv'
 import { TruepointStack } from '../lib/truepoint-production-stack';
+import { TruepointDevStack } from '../lib/truepoint-dev-stack';
+import { WhileTrueCollectorStack } from '../lib/truepoint-collectorDB-stack';
+import { WhileTrueTruepointVpcStack } from '../lib/vpc-stack';
 
 dotenv.config();
 
 const app = new cdk.App();
-new TruepointStack(app, 'TruepointStack', {
-  env: {
-    account: process.env.AWS_ACCOUNT_ID,
-    region: process.env.AWS_ONAD_REGION,
-  }
-});
+const env = {
+  account: process.env.AWS_ACCOUNT_ID,
+  region: process.env.AWS_ONAD_REGION,
+}
+
+// **********************************
+// Truepoint Development Stack
+// **********************************
+new TruepointDevStack(app, 'TruepointDev', { env });
+
+
+// **********************************
+// VPC stack
+// **********************************
+// const TrupointVpcStack = new WhileTrueTruepointVpcStack(app, 'WhileTrue', { env });
+
+// **********************************
+// Collector DB Stack
+// **********************************
+// new WhileTrueCollectorStack(
+//   app,
+//   'WhileTrueCollector',
+//   { env, vpc: TrupointVpcStack.vpc }
+// );
+
+// **********************************
+// Truepoint Production Stack
+// **********************************
+// new TruepointStack(
+//   app,
+//   'TruepointProduction',
+//   { env, vpc: TrupointVpcStack.vpc }
+// );
--- aws/cdk/utils/getParams.ts
+++ aws/cdk/utils/getParams.ts
@@ -4,7 +4,7 @@ import * as ssm from '@aws-cdk/aws-ssm';
 export default function getSSMParams(scope: cdk.Construct) {
   
   // *****************************************************
-  // Db configurations
+  // Production DB configurations
   const TRUEPOINT_DB_HOST = ssm.StringParameter.fromStringParameterAttributes(
     scope, 'TRUEPOINT/DB_HOST', {
       parameterName: '/TRUEPOINT/DB_HOST'
@@ -38,6 +38,41 @@ export default function getSSMParams(scope: cdk.Construct) {
     }
   );
 
+  // *****************************************************
+  // Development DB configurations
+  const DEV_TRUEPOINT_DB_HOST = ssm.StringParameter.fromStringParameterAttributes(
+    scope, 'TRUEPOINT/DEV/DB_HOST', {
+      parameterName: '/TRUEPOINT/DEV/DB_HOST'
+    }
+  );
+  const DEV_TRUEPOINT_DB_PASSWORD = ssm.StringParameter.fromSecureStringParameterAttributes(
+    scope, 'TRUEPOINT/DEV/DB_PASSWORD', {
+      parameterName: '/TRUEPOINT/DEV/DB_PASSWORD',
+      version: 1
+    }
+  );
+  const DEV_TRUEPOINT_DB_PORT = ssm.StringParameter.fromSecureStringParameterAttributes(
+    scope, 'TRUEPOINT/DEV/DB_PORT', {
+      parameterName: '/TRUEPOINT/DEV/DB_PORT',
+      version: 1
+    }
+  );
+  const DEV_TRUEPOINT_DB_USER = ssm.StringParameter.fromStringParameterAttributes(
+    scope, 'TRUEPOINT/DEV/DB_USER', {
+      parameterName: '/TRUEPOINT/DEV/DB_USER'
+    }
+  );
+  const DEV_TRUEPOINT_DB_DATABASE = ssm.StringParameter.fromStringParameterAttributes(
+    scope, 'TRUEPOINT/DEV/DB_DATABASE', {
+      parameterName: '/TRUEPOINT/DEV/DB_DATABASE'
+    }
+  );
+  const DEV_TRUEPOINT_DB_CHARSET = ssm.StringParameter.fromStringParameterAttributes(
+    scope, 'TRUEPOINT/DEV/DB_CHARSET', {
+      parameterName: '/TRUEPOINT/DEV/DB_CHARSET',
+    }
+  );
+
   
   // Onad User crypto cipher credentials
   const TRUEPOINT_CIPHER_IV = ssm.StringParameter.fromSecureStringParameterAttributes(
@@ -58,6 +93,12 @@ export default function getSSMParams(scope: cdk.Construct) {
     TRUEPOINT_DB_USER,
     TRUEPOINT_DB_DATABASE,
     TRUEPOINT_DB_CHARSET,
+    DEV_TRUEPOINT_DB_HOST,
+    DEV_TRUEPOINT_DB_PASSWORD,
+    DEV_TRUEPOINT_DB_PORT,
+    DEV_TRUEPOINT_DB_USER,
+    DEV_TRUEPOINT_DB_DATABASE,
+    DEV_TRUEPOINT_DB_CHARSET,
     TRUEPOINT_CIPHER_IV,
     TRUEPOINT_CIPHER_KEY,
   };
