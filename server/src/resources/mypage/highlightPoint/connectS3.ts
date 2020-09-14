61f43962c7e1699aa0651c9e8f742f04d574362f
leejineun <kevin2022@naver.com>
2020-09-14 17:16:01 +0900

    s3 testpage 주석처리

--- server/src/resources/mypage/highlightPoint/connectS3.ts
+++ server/src/resources/mypage/highlightPoint/connectS3.ts
@@ -1,28 +1,22 @@
-import * as AWS from 'aws-sdk';
-import * as dotenv from 'dotenv';
+// import * as AWS from 'aws-sdk';
+// import * as dotenv from 'dotenv';
 
-dotenv.config();
-const s3 = new AWS.S3();
+// dotenv.config();
+// const s3 = new AWS.S3();
 
-console.log(process.env.AWS_ACCESS_KEY_ID);
-const getParams = {
-  Bucket: 'truepoint', // your bucket name,
-  Key: 'highlight_json/134859149/2020-09-13/39667416302/39667416302' // path to the object you're looking for
-};
-
-s3.getObject(getParams, (err, data) => {
-  if (err) {
-    console.log(err, err.stack);
-  } else {
-    const objectData = data.Body.toString('utf-8'); // Use the encoding necessary
-    console.log(objectData);
-  }
-  console.log('JOB DONE');
-});
-// s3.listBuckets((err, data) => {
+// s3.getObject(getParams, (err, data) => {
 //   if (err) {
-//     console.log('Error', err);
+//     console.log(err, err.stack);
 //   } else {
-//     console.log('Success', data.Buckets);
+//     const objectData = data.Body.toString('utf-8'); // Use the encoding necessary
+//     console.log(objectData);
 //   }
+//   console.log('JOB DONE');
 // });
+// // s3.listBuckets((err, data) => {
+// //   if (err) {
+// //     console.log('Error', err);
+// //   } else {
+// //     console.log('Success', data.Buckets);
+// //   }
+// // });
