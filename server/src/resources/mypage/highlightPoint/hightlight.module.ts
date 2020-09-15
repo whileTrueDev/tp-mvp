f07ea91dae5488e00af430a8ae260fbeb09c7f97
leejineun <kevin2022@naver.com>
2020-09-15 10:22:17 +0900

    커밋되돌린 후 재푸시

--- server/src/resources/mypage/highlightPoint/hightlight.module.ts
+++ server/src/resources/mypage/highlightPoint/hightlight.module.ts
@@ -1,11 +1,10 @@
-// import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
 // import { HighlightService } from './highlight.service';
-// import { HighlightController } from './highlight.controller';
-
-// @Module({
-//   providers: [HighlightService],
-//   controllers: [HighlightController],
-//   exports: [HighlightService]
-// })
-// export class UsersModule { }
+import { HighlightController } from './highlight.controller';
+import { HighlightService } from './highlight.service';
+@Module({
+  providers: [HighlightService],
+  controllers: [HighlightController],
+  exports: [HighlightService]
+})
+export class HighlightModule { }
