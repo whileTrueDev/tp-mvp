865b3b02bc95faa831f7bdbd3e0384ca5a5aad41
leejineun@gmail.com <mbaku@DESKTOP-BQDDH3L.localdomain>
2020-09-18 15:05:47 +0900

    metrics변경사항 적용

--- analyzer/highlightAnalyzer/lib/s3_connector.py
+++ analyzer/highlightAnalyzer/lib/s3_connector.py
@@ -4,34 +4,43 @@ import json
 from botocore.exceptions import ClientError
 
 
-def upload_file(file_name, bucket, object_name=None):
-    """Upload a file to an S3 bucket
+class S3Connector:
 
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
+    def __init__(self, bucket_name, stream_id):
+        self.s3 = boto3.resource('s3')
+        self.bucket_name = bucket_name
+        self.stream_id = stream_id
 
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = file_name
+    # def upload_file(self, bucket, object_name=None):
+    #     """Upload a file to an S3 bucket
 
-    # Upload the file
-    s3_client = boto3.client('s3')
-    try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
+    #     :param file_name: File to upload
+    #     :param bucket: Bucket to upload to
+    #     :param object_name: S3 object name. If not specified then file_name is used
+    #     :return: True if file was uploaded, else False
+    #     """
 
+    #     # If S3 object_name was not specified, use file_name
+    #     if object_name is None:
+    #         object_name = file_name
 
-def upload_json(bucket_name, json_to_dump, path, stream_id):
-    s3 = boto3.resource('s3')
-    s3object = s3.Object(bucket_name, '{path}/{stream_id}'.format(path=path, stream_id=stream_id))
+    #     # Upload the file
+    #     s3_client = boto3.client('s3')
+    #     try:
+    #         response = s3_client.upload_file(file_name, bucket, object_name)
+    #     except ClientError as e:
+    #         logging.error(e)
+    #         return False
+    #     return True
 
-    s3object.put(
-        Body=(bytes(json.dumps(json_to_dump).encode('UTF-8'))),
-        ContentType='application/json'
-    )
+    def upload_json(self, json_to_dump, path, stream_id):
+        s3object = self.s3.Object(self.bucket_name, '{path}/{stream_id}'.format(path=path, stream_id=self.stream_id))
+
+        s3object.put(
+            Body=(bytes(json.dumps(json_to_dump).encode('UTF-8'))),
+            ContentType='application/json'
+        )
+
+    def upload_srt(self, path, json_to_save, file_name):
+        s3object = self.s3.Object(self.bucket_name, '{path}/{stream_id}'.format(path=path, stream_id=self.stream_id))
+        s3object.put(Key='{file_name}.srt'.format(file_name=file_name), Body=json_to_save)
--- analyzer/highlightAnalyzer/run.py
+++ analyzer/highlightAnalyzer/run.py
@@ -3,7 +3,7 @@
 # 불러온 데이터를 전처리할 수 있는 형태로 바꾼다.
 # 전처리를 한다
 # 분석을 한다
-# json 형태의 output으로 반환한다
+# json 형태의 highlight_json으로 반환한다
 # s3에 저장한다
 import re
 import datetime
@@ -11,11 +11,12 @@ import pandas as pd
 import numpy as np
 from datetime import date
 from multiprocessing import Process, Pool
-from lib.s3_connector import upload_json
+from lib.s3_connector import S3Connector
 from lib.db_connect import DbHandler
 from lib.morphs_analyzer import morphs_analyzer, word_counter, get_line_score, morphs_test
 from lib.__json import json_dumper
 from lib.warning_eliminator import ChainedAssignent
+from lib.file_converter import FileConverter
 
 
 def main(data, platform, stream_id):
@@ -24,8 +25,16 @@ def main(data, platform, stream_id):
     platform : 채팅이 담겨져 있는 DB table 명
     stream_id : 해당 채팅의 streamId 
     """
-    output = {'videoId': '', 'end_date': '', 'total_index': 0, 'highlight_points': []}
-    output2 = {'videoId': '', 'end_date': '', 'total_index': '', 'metrics': {}}
+    highlight_json = {'videoId': '', 'start_date': '', 'end_date': '', 'total_index': 0, 'highlight_points': []}
+    metrics_json = {
+        "videoId": "",
+        "start_date": "",
+        "end_date": "",
+                    "total_index": 0,
+                    "time_line": [],
+                    "chat_points": [],
+                    "smile_points": []
+    }
     # 데이터프레임화
     df = pd.DataFrame(data)
     if platform == 'AfreecaStreams':
@@ -73,10 +82,11 @@ def main(data, platform, stream_id):
             with ChainedAssignent():
                 df_highlight['group'].iloc[row] = group_num
     # videoid, end_date, 총 index 값 넣기
-    output['videoId'] = stream_id
-    output['end_date'] = df_list.index[-1].strftime('%Y-%m-%d')
-    output['total_index'] = len(df_list)
-    # 하이라이트 시간을 가진 그룹과 인덱스를 가진 그룹으로 만들어서 output에 넣어준다
+    highlight_json['videoId'] = stream_id
+    highlight_json['start_date'] = df_list.index[0].strftime('%Y-%m-%d %H:%M:%S')
+    highlight_json['end_date'] = df_list.index[-1].strftime('%Y-%m-%d')
+    highlight_json['total_index'] = len(df_list)
+    # 하이라이트 시간을 가진 그룹과 인덱스를 가진 그룹으로 만들어서 highlight_json에 넣어준다
     highlight_group = df_highlight.index.groupby(df_highlight['group'])
     if len(highlight_group) == 0:
         print('too short to analyze break the process')
@@ -92,7 +102,35 @@ def main(data, platform, stream_id):
         highlight_point['end_time'] = str(highlight_group[v][-1])
         highlight_point['start_index'] = str(index_group[i][0])
         highlight_point['end_index'] = str(index_group[i][-1])
-        output['highlight_points'].append(highlight_point)
+        highlight_json['highlight_points'].append(highlight_point)
+        highlight_point = {}
+    # metrics json
+    chat_points, smile_points = [[], []]
+    metrics_counts = {}
+    for i in range(len(df_onair)):
+        metrics_counts['smile_count'] = df_onair['count'][i]
+        metrics_counts['chat_count'] = df_onair['ㅋ'][i]
+        metrics_json['time_line'].append(metrics_counts)
+        metrics_counts = {}
+    chat_count = list(df_onair['count'])
+    smile_count = list(df_onair['ㅋ'])
+    chat_points = list(df_highlight['count'])
+    smile_points = list(df_highlight['ㅋ'])
+    metrics_json['videoId'] = stream_id
+    metrics_json['start_date'] = df_list.index[0].strftime('%Y-%m-%d %H:%M:%S')
+    metrics_json['end_date'] = df_list.index[-1].strftime('%Y-%m-%d %H:%M:%S')
+    metrics_json['total_index'] = len(df_list)
+    metrics_json['chat_points'] = chat_points
+    metrics_json['smile_points'] = smile_points
+    creator_id = df['creatorId'].unique()[0]
+    today = date.today().strftime("%Y-%m-%d")
+
+    for i, v in enumerate(reversed(list(highlight_group.keys()))):
+        highlight_point['start_time'] = str(highlight_group[v][0])
+        highlight_point['end_time'] = str(highlight_group[v][-1])
+        highlight_point['start_index'] = str(index_group[i][0])
+        highlight_point['end_index'] = str(index_group[i][-1])
+        highlight_json['highlight_points'].append(highlight_point)
         highlight_point = {}
     # metrics json
     chat_count, chat_points, smile_count, smile_points = [[], [], [], []]
@@ -100,38 +138,53 @@ def main(data, platform, stream_id):
     smile_count = list(df_onair['ㅋ'])
     chat_points = list(df_highlight['count'])
     smile_points = list(df_highlight['ㅋ'])
-    output2['videoId'] = stream_id
-    output2['end_date'] = df_list.index[-1].strftime('%Y-%m-%d')
-    output2['total_index'] = len(df_list)
-    output2['metrics']['chat_count'] = chat_count
-    output2['metrics']['chat_points'] = chat_points
-    output2['metrics']['smile_count'] = smile_count
-    output2['metrics']['smile_points'] = smile_points
+    metrics_json['videoId'] = stream_id
+    metrics_json['end_date'] = df_list.index[-1].strftime('%Y-%m-%d')
+    metrics_json['total_index'] = len(df_list)
+    metrics_json['metrics']['chat_count'] = chat_count
+    metrics_json['metrics']['chat_points'] = chat_points
+    metrics_json['metrics']['smile_count'] = smile_count
+    metrics_json['metrics']['smile_points'] = smile_points
     creator_id = df['creatorId'].unique()[0]
     today = date.today().strftime("%Y-%m-%d")
-
-    upload_json('truepoint', output, 'highlight_json/{creator_id}/{date}/{stream_id}'.format(creator_id=creator_id, date=today, stream_id=stream_id), stream_id)
-    upload_json('truepoint', output2, 'metrics_json/{creator_id}/{date}/{stream_id}'.format(creator_id=creator_id, date=today, stream_id=stream_id), stream_id)
+    # s3 = S3Connector()
+    # s3.upload_json('truepoint', highlight_json, 'highlight_json/{creator_id}/{date}/{stream_id}'.format(creator_id=creator_id, date=today, stream_id=stream_id), stream_id)
+    # s3.upload_json('truepoint', metrics_json, 'metrics_json/{creator_id}/{date}/{stream_id}'.format(creator_id=creator_id, date=today, stream_id=stream_id), stream_id)
 
 
 if __name__ == '__main__':
-    # db state 확인해서 최근 종료된 방송이 있는 지 확인한다
-    platform_list = [('AfreecaStreams'), ('TwitchStreams'), ('YoutubeStreams')]
-    db_conn = DbHandler()
-    with Pool(processes=3) as pool:
-        to_analyze = pool.map(db_conn.check_state, platform_list)
-    afreeca_bool, twitch_bool, youtube_bool = [bool(to_analyze[0]['AfreecaStreams']), bool(to_analyze[1]['TwitchStreams']), bool(to_analyze[2]['YoutubeStreams'])]
-    data = []
-    for stream in to_analyze:
-        platform = list(stream.keys())[0]
-        for stream_id in list(stream.values())[0]:
-            if (afreeca_bool or twitch_bool or youtube_bool):
-                data.append(db_conn.twitch_chat_call(stream_id, platform))
+    highlight_json = {'videoId': '39667416302', 'start_date': '2020-09-13', 'end_date': '2020-09-13', 'total_index': 92,
+                      'highlight_points': [{'start_time': '2020-09-13 21:55:00', 'end_time': '2020-09-13 21:55:00', 'start_index': 7, 'end_index': 10},
+                                           {'start_time': '2020-09-13 21:56:00', 'end_time': '2020-09-13 21:56:00', 'start_index': 34, 'end_index': 54},
+                                           {'start_time': '2020-09-13 21:58:30', 'end_time': '2020-09-13 21:58:30', 'start_index': 57, 'end_index': 62}]}
+    converter = FileConverter()
+    srt = converter.return_srt(highlight_json)
+    bucket_name = 'truepoint'
+    stream_id = '123123123'
 
-        for i, line in enumerate(data):
-            platform = list(line.keys())[0]
-            if isinstance(list(line.values())[0], (list)):
-                # 유튜브는 streamId == creatorId
-                stream_id = line[platform][0]['streamId']
-                main(line[platform], platform, stream_id)
-                db_conn.update_state(platform, stream_id)
+    s3 = S3Connector(bucket_name, stream_id)
+    creator_id = '134859149'
+    today = '2020_09_18'
+    srt_file_name = '{name}일 방송 하이라이트'.format(name=highlight_json['start_date'])
+    s3.upload_srt('highlight_json/{creator_id}/{date}/{stream_id}'.format(creator_id=creator_id, date=today, stream_id=stream_id), srt, srt_file_name)
+    # s3.upload_json('truepoint', metrics_json, 'metrics_json/{creator_id}/{date}/{stream_id}'.format(creator_id=creator_id, date=today, stream_id=stream_id), stream_id)
+    # db state 확인해서 최근 종료된 방송이 있는 지 확인한다
+    # platform_list = [('AfreecaStreams'), ('TwitchStreams'), ('YoutubeStreams')]
+    # db_conn = DbHandler()
+    # with Pool(processes=3) as pool:
+    #     to_analyze = pool.map(db_conn.check_state, platform_list)
+    # afreeca_bool, twitch_bool, youtube_bool = [bool(to_analyze[0]['AfreecaStreams']), bool(to_analyze[1]['TwitchStreams']), bool(to_analyze[2]['YoutubeStreams'])]
+    # data = []
+    # for stream in to_analyze:
+    #     platform = list(stream.keys())[0]
+    #     for stream_id in list(stream.values())[0]:
+    #         if (afreeca_bool or twitch_bool or youtube_bool):
+    #             data.append(db_conn.twitch_chat_call(stream_id, platform))
+
+    #     for i, line in enumerate(data):
+    #         platform = list(line.keys())[0]
+    #         if isinstance(list(line.values())[0], (list)):
+    #             # 유튜브는 streamId == creatorId
+    #             stream_id = line[platform][0]['streamId']
+    #             main(line[platform], platform, stream_id)
+    #             # db_conn.update_state(platform, stream_id)
