3a747660af0ce5c07f2358d730c1f5ae59815f12
hwasurr <iamsupermazinga@gmail.com>
2020-11-02 10:59:15 +0900

    페이지 사이즈 경고 알림 텍스트 크기 수정

--- client/web/src/organisms/mypage/alertbar/PageSizeAlert.tsx
+++ client/web/src/organisms/mypage/alertbar/PageSizeAlert.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Alert } from '@material-ui/lab';
-import { useMediaQuery } from '@material-ui/core';
+import { Typography, useMediaQuery } from '@material-ui/core';
 
 export interface PageSizeAlertProps {
   open?: boolean;
@@ -27,7 +27,9 @@ export default function PageSizeAlert({
           borderRadius: 0, position: 'fixed', zIndex: 2000, width: '100%', top: 80,
         }}
       >
-        지원하지 않는 화면 사이즈로, 화면이 올바르게 표현되지 않을 수 있습니다. PC 화면을 사용하시기를 권장드립니다.
+        <Typography variant="body1">
+          지원하지 않는 화면 사이즈로, 화면이 올바르게 표현되지 않을 수 있습니다. PC 화면을 사용하시기를 권장드립니다.
+        </Typography>
       </Alert>
       )}
     </>
