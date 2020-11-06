295cc99eed7ee48443eff6cb30d9472bbd42220e
BEOBWOO <qjqdn1568@gmail.com>
2020-11-06 10:04:49 +0900

    유저 엔터티와 알림 엔터티 1:N 관계 설정

--- server/src/resources/notification/entities/notification.entity.ts
+++ server/src/resources/notification/entities/notification.entity.ts
@@ -1,7 +1,10 @@
 import { Notification } from '@truepoint/shared/dist/interfaces/Notification.interface';
 import {
-  Entity, Column, PrimaryGeneratedColumn, CreateDateColumn,
+  Entity, Column, PrimaryGeneratedColumn,
+  CreateDateColumn, ManyToOne, JoinColumn,
 } from 'typeorm';
+
+import { UserEntity } from '../../users/entities/user.entity';
 
 @Entity({ name: 'Notification' })
 export class NotificationEntity implements Notification {
@@ -25,6 +28,10 @@ export class NotificationEntity implements Notification {
 
   @CreateDateColumn()
   createdAt: Date;
+
+  @ManyToOne((type) => UserEntity, (user) => user.notification)
+  @JoinColumn({ name: 'userId' })
+  user: UserEntity
 
   constructor(partial: Partial<NotificationEntity>) {
     Object.assign(this, partial);
