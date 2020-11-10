2a788e8e1165b7314a03b2a001428f953c3ede03
Emma_sangeun <peng1650@naver.com>
2020-11-10 09:44:31 +0900

    워드클라우드 오류수정

--- client/web/src/pages/mypage/channel-analysis/VideoAnalysis.tsx
+++ client/web/src/pages/mypage/channel-analysis/VideoAnalysis.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import MypageSectionWrapper from '../../../atoms/MypageSectionWrapper';
-import WordCloud from '../../../organisms/shared/WordCloud';
 
 export default function VideoAnalysis(): JSX.Element {
   return (
     <div>
       <MypageSectionWrapper>VideoAnalysis</MypageSectionWrapper>
-      <WordCloud />
     </div>
   );
 }
