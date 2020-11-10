b61155c13428fd08364622ccc57379e6e7cb7c81
Emma_sangeun <peng1650@naver.com>
2020-11-10 10:08:21 +0900

    워드클라우드 컴포넌트 생성

--- client/web/src/organisms/shared/WordCloud.tsx
+++ client/web/src/organisms/shared/WordCloud.tsx
@@ -3,11 +3,25 @@ import ReactWordcloud from 'react-wordcloud';
 
 import 'tippy.js/dist/tippy.css';
 import 'tippy.js/animations/scale.css';
+import { makeStyles, Grid } from '@material-ui/core';
 import dummy from './dummy';
 
 type MinMaxPair = [number, number];
+type scale = 'undefined' | 'linear' | 'sqrt' | 'log' ;
+const wordScale: scale = 'sqrt';
 const sizeNum: MinMaxPair = [5, 60];
 const rotNum: MinMaxPair = [0, 0];
+const useStyles = makeStyles(() => ({
+  root: {
+    marginLeft: 350,
+    height: 950,
+    width: 1200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    justifyItems: 'center',
+    alignContent: 'center',
+  },
+}));
 const options = {
   colors: ['#4b5ac7', '#929ef8'],
   enableTooltip: true,
@@ -17,19 +31,22 @@ const options = {
   fontStyle: 'normal',
   fontWeight: 'normal',
   padding: 1,
+  scale: wordScale,
   rotations: 3,
   rotationAngles: rotNum,
-  scale: 'sqrt',
-  spiral: 'archimedean',
   transitionDuration: 1000,
 };
 
 function WordCloud(): JSX.Element {
+  const classes = useStyles();
   return (
     <div>
-      <p>Configure options in the code editor!</p>
-      <div style={{ height: 400, width: 600 }}>
-        <ReactWordcloud options={options} words={dummy} />
+      <div className={classes.root}>
+        <Grid container>
+          <Grid item xs={12} lg={10}>
+            <ReactWordcloud options={options} words={dummy} />
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
