97215470f066cec482842485bdd20c9ddb8513d1
김법우 <qjqdn1568@naver.com>
2020-12-01 15:03:22 +0900

    마케팅 문구 반영 , 회원탈퇴 다이얼로그 문구수정

--- client/web/src/organisms/mypage/layouts/HighlightAnalysisLayout.tsx
+++ client/web/src/organisms/mypage/layouts/HighlightAnalysisLayout.tsx
@@ -10,6 +10,7 @@ import IconButton from '@material-ui/core/IconButton';
 import { useSnackbar } from 'notistack';
 import classnames from 'classnames';
 import ClearIcon from '@material-ui/icons/Clear';
+import Card from '@material-ui/core/Card';
 import Calendar from '../highlightAnalysis/Calendar';
 import Button from '../../../atoms/Button/Button';
 import useHighlightAnalysisLayoutStyles from './HighlightAnalysisLayout.style';
