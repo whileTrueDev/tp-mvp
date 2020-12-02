cfcba42c9de1c5f3fa0686dbdda445417d3bb461
KIMBEOBWOO <qjqdn1568@naver.com>
2020-12-02 15:52:31 +0900

    conflict resolve

--- client/web/src/organisms/mypage/layouts/HighlightAnalysisLayout.tsx
+++ client/web/src/organisms/mypage/layouts/HighlightAnalysisLayout.tsx
@@ -11,7 +11,6 @@ import IconButton from '@material-ui/core/IconButton';
 import { useSnackbar } from 'notistack';
 import classnames from 'classnames';
 import ClearIcon from '@material-ui/icons/Clear';
-import Card from '@material-ui/core/Card';
 import Calendar from '../highlightAnalysis/Calendar';
 import Button from '../../../atoms/Button/Button';
 import useHighlightAnalysisLayoutStyles from './HighlightAnalysisLayout.style';
