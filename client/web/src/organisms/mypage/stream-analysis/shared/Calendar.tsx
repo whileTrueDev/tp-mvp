c87cc2149b25b2c6b6486104768c46f559f7003e
KIMBEOBWOO <qjqdn1568@naver.com>
2020-12-02 17:10:34 +0900

    달력 스타일 파일 분리 + userId 체크 코드 삭제

--- client/web/src/organisms/mypage/stream-analysis/shared/Calendar.tsx
+++ client/web/src/organisms/mypage/stream-analysis/shared/Calendar.tsx
@@ -10,74 +10,19 @@ import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
 import koLocale from 'date-fns/locale/ko';
 import DateFnsUtils from '@date-io/date-fns';
 // styles
-import { makeStyles, Theme } from '@material-ui/core/styles';
+import { Theme } from '@material-ui/core/styles';
 import classnames from 'classnames';
 import moment from 'moment';
 // interfaces
 import { CalendarProps } from './StreamAnalysisShared.interface';
-
-const useStyles = makeStyles((theme: Theme) => ({
-  leftCircleBase: {
-    width: '50%',
-    backgroundColor: '#d7e7ff',
-  },
-  leftCircleCompare: {
-    width: '50%',
-    backgroundColor: '#d3d19d',
-  },
-  rigthCircleBase: {
-    background: `linear-gradient(to left,#d7e7ff 50%, ${theme.palette.background.paper} 50%)`,
-  },
-  rigthCircleCompare: {
-    background: `linear-gradient(to left,#d3d19d 50%, ${theme.palette.background.paper} 50%)`,
-  },
-  rangeDayBase: {
-    backgroundColor: '#d7e7ff',
-  },
-  rangeDayCompare: {
-    backgroundColor: '#d3d19d',
-  },
-  selectedDayBase: {
-    backgroundColor: '#d7e7ff',
-  },
-  selectedDayCompare: {
-    backgroundColor: '#d3d19d',
-  },
-  hasStreamDayDotContainer: {
-    position: 'relative',
-  },
-  hasStreamDayDotBase: {
-    position: 'absolute',
-    height: 0,
-    width: 0,
-    border: '3px solid',
-    borderRadius: 5,
-    borderColor: '#3a86ff',
-    right: '44%',
-    transform: 'translateX(1px)',
-    top: '80%',
-    backGroundColor: '#3a86ff',
-  },
-  hasStreamDayDotCompare: {
-    position: 'absolute',
-    height: 0,
-    width: 0,
-    border: '3px solid',
-    borderRadius: 5,
-    borderColor: '#b1ae71',
-    right: '44%',
-    transform: 'translateX(1px)',
-    top: '80%',
-    backGroundColor: '#b1ae71',
-  },
-}));
+import useAllCalendarStyles from './RangeSelectCalendar.style';
 
 function Calendar(props: CalendarProps): JSX.Element {
   const {
     period, base, handleSelectedDate, currDate, selectedStreams,
   } = props;
 
-  const classes = useStyles();
+  const classes = useAllCalendarStyles();
   const [point1, setPoint1] = React.useState<MaterialUiPickersDate>(period[0]);
   const [point2, setPoint2] = React.useState<MaterialUiPickersDate>(period[1]);
 
