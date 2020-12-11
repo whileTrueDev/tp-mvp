2b416f4da0004eb0c10b05f8a4f8e2bad87e0a97
hwasurr <iamsupermazinga@gmail.com>
2020-12-11 13:29:23 +0900

    lint 오류 제거

--- client/web/src/organisms/mypage/dashboard/UserProfile.tsx
+++ client/web/src/organisms/mypage/dashboard/UserProfile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import classnames from 'classnames';
 import {
   Avatar, Chip, CircularProgress, Paper, Typography,
