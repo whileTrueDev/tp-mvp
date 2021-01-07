e2bb143872e73e5753e417ea5632de4ed8edb5d1
jiyeon lee <a1919361@gmail.com>
2021-01-07 14:45:17 +0900

    버

--- client/web/src/organisms/joniPilot/VideoAnalysisReaction.tsx
+++ client/web/src/organisms/joniPilot/VideoAnalysisReaction.tsx
@@ -1,12 +1,122 @@
-import React from 'react';
+import React, {
+  useState, useEffect, useMemo, useCallback,
+} from 'react';
+import { Button, Grid } from '@material-ui/core';
+import axios from 'axios';
+
+import MaterialTable from '../../atoms/Table/MaterialTable';
 import ChannelAnalysisSectionLayout from './ChannelAnalysisSectionLayout';
 
-export default function VideoAnalysisReaction(): JSX.Element {
-  return (
+interface fakeCommentType{
+  commentId: string;
+  commentText: string;
+  streamId: string;
+  createdAt: Date;
+  userId: string;
+  nickname: string;
+  profileImage: string;
+  likes: number;
+  hates: number;
+  replies: number;
+}
+const url = {
+  mostPopular: 'http://localhost:4000/mostPopularComments?_sort=likes&_order=desc',
+  mostPopularReplies: 'http://localhost:4000/mostPopularReplies?_sort=likes&_order=desc',
+  mostSmiledComments: 'http://localhost:4000/mostSmiledComments?_sort=likes&_order=desc',
+  feedbackComments: 'http://localhost:4000/feedbackComments?_sort=replies&_order=desc',
+};
+const commentTableOptions = {
+  toolbar: false,
+  showFirstLastPageButtons: false,
+  search: false,
+  showTitle: false,
+  draggable: false,
+  sorting: false,
+  paging: false,
+  headerStyle: {
+    textAlign: 'center' as const,
+  },
+};
 
+export default function VideoAnalysisReaction(): JSX.Element {
+  const [comments, setComments] = useState<fakeCommentType[]|null>(null);
+
+  useEffect(() => {
+    getComments(url.mostPopular)();
+  }, []);
+
+  const getComments = useCallback((commentUrl) => async () => {
+    try {
+      const response = await axios.get(commentUrl);
+      setComments(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  const buttonData = [
+    {
+      label: '가장 인기 많은 댓글',
+      onClick: getComments(url.mostPopular),
+    },
+    {
+      label: '가장 인기 많은 대댓글',
+      onClick: getComments(url.mostPopularReplies),
+    },
+    {
+      label: '웃음이 많은 댓글',
+      onClick: getComments(url.mostSmiledComments),
+    },
+    {
+      label: '피드백 댓글',
+      onClick: getComments(url.feedbackComments),
+    },
+  ];
+
+  const commentsData = useMemo(() => {
+    if (comments) {
+      return comments.map((comment) => (
+        {
+          date: comment.createdAt,
+          commentInfo: comment.commentText,
+          likes: comment.likes,
+          hates: comment.hates,
+          replies: comment.replies,
+        }
+      ));
+    }
+    return [];
+  }, [comments]);
+
+  const commentTableColumns = [
+    { field: 'date', title: '날짜' },
+    { field: 'commentInfo', title: '' },
+    { field: 'likes', title: '좋아요' },
+    { field: 'hates', title: '싫어요' },
+    { field: 'replies', title: '대댓글' },
+  ];
+  return (
     <ChannelAnalysisSectionLayout
       title="반응 분석"
       tooltip="반응분석~~~"
-    />
+    >
+      <Grid container direction="row" justify="space-between">
+        {buttonData.map((button) => (
+          <Grid item>
+            <Button
+              variant="outlined"
+              onClick={button.onClick}
+            >
+              {button.label}
+            </Button>
+          </Grid>
+        ))}
+      </Grid>
+      <MaterialTable
+        columns={commentTableColumns}
+        data={commentsData}
+        options={commentTableOptions}
+      />
+    </ChannelAnalysisSectionLayout>
   );
 }
--- client/web/src/pages/joniPilot/data.js
+++ client/web/src/pages/joniPilot/data.js
@@ -1,72 +1,23 @@
-// function makeRandomColor() {
-//   return Math.floor(Math.random() * 16777215).toString(16);
-// }
+function makeRandomColor() {
+  return Math.floor(Math.random() * 16777215).toString(16);
+}
 
-// function getRandomDate(from, to) {
-//   const fromTime = from.getTime();
-//   const toTime = to.getTime();
-//   return new Date(fromTime + Math.random() * (toTime - fromTime));
-// }
+function getRandomDate(from, to) {
+  const fromTime = from.getTime();
+  const toTime = to.getTime();
+  return new Date(fromTime + Math.random() * (toTime - fromTime));
+}
 const getSerializedDate = (from, day) => new Date(new Date(from).getTime() - (day * 8.64e+7)).toISOString();
 module.exports = () => {
   const data = {
     videos: [],
-    comments: [],
+    mostPopularComments: [],
+    mostPopularReplies: [],
+    mostSmiledComments: [],
+    feedbackComments: [],
     words: [],
-    streams: [
-      {
-        streamId: 'id1',
-        startDate: new Date('2020-12-12'),
-        endDate: new Date('2020-12-13'),
-        creatorId: 'asdf',
-        title: 'wstream title',
-        platform: 'afreeca',
-        airTime: 1234,
-        isRemoved: false,
-        chatCount: 1,
-        smileCount: 2,
-        viewer: 3,
-      },
-      {
-        streamId: 'id123',
-        startDate: new Date('2020-12-13'),
-        endDate: new Date('2020-12-14'),
-        creatorId: 'asd1111f',
-        title: 'wstre11am title',
-        platform: 'twitch',
-        airTime: 122234,
-        isRemoved: false,
-        chatCount: 123,
-        smileCount: 2,
-        viewer: 3,
-      },
-      {
-        streamId: 'id12223',
-        startDate: new Date('2020-12-12'),
-        endDate: new Date('2020-12-13'),
-        creatorId: 'asd123f',
-        title: 'wstream ti2222tle',
-        platform: 'youtube',
-        airTime: 12323334,
-        isRemoved: false,
-        smileCount: 2123,
-        chatCount: 1,
-        viewer: 3,
-      },
-    ],
   };
 
-  /**
-    platform: 'afreeca' | 'youtube' | 'twitch';
-    airTime: number;
-    startDate: Date;
-    endDate: Date;
-    creatorId: string;
-    smileCount: number;
-    viewer: number;
-    chatCount: number;
-    needAnalysis: boolean;
-   */
   for (let i = 0; i < 50; i += 1) {
     const videoItem = {
       streamId: `streamId-${i}`,
@@ -79,11 +30,10 @@ module.exports = () => {
       viewer: i * 100,
       chatCount: i * 100,
       needAnalysis: false,
-      // thumbnail: `https://dummyimage.com/100x100/${makeRandomColor()}/fff&text=dummy`,
       thumbnail: `https://img.youtube.com/vi/${i % 2 === 0 ? 'CyqctquwO3Y' : 'kvJoIpjoY-0'}`,
       views: 10000000 - (i * 33333),
-      likes: (i) * 10000,
-      hates: (i) * 100,
+      likes: (i + 1) * 143,
+      hates: (i + 2) * 87,
       rating: Math.floor(Math.random() * 5),
       tags: ['블랙핑크', '블핑', 'k-pop', 'yg'],
       comments: 1000,
@@ -91,19 +41,23 @@ module.exports = () => {
     data.videos.push(videoItem);
   }
 
-  // for (let i = 0; i < 100; i += 1) {
-  //   const commentItem = {
-  //     id: i,
-  //     date: generateRandomDOB(),
-  //     user: {
-  //       avatarImage: `https://dummyimage.com/50x50/${makeRandomColor()}/fff&text=${i}`,
-  //       nickname: `user-${i}`,
-  //     },
-  //     likes: Math.floor(Math.random() * 10000000000),
-  //     hates: Math.floor(Math.random() * 10000000000),
-  //     videoId: Math.floor(Math.random() * 30),
-  //   };
-  //   data.comments.push(commentItem);
-  // }
+  for (let i = 0; i < 5; i += 1) {
+    const commentItem = {
+      commentId: `comment-${i}`,
+      commentText: '',
+      streamId: `stream-${i}`,
+      createdAt: getRandomDate(new Date(new Date().getTime() - (i * 8.64e+7)), new Date()),
+      userId: `user-${i}`,
+      nickname: `user-nickname-${i}`,
+      profileImage: `https://dummyimage.com/50x50/${makeRandomColor()}/fff&text=${i}`,
+      likes: Math.floor(Math.random() * 100000),
+      hates: Math.floor(Math.random() * 10000),
+      replies: Math.floor(Math.random() * 30),
+    };
+    data.mostPopularComments.push({ ...commentItem, commentText: `가장 인기 많은 댓글 ${i}` });
+    data.mostPopularReplies.push({ ...commentItem, commentText: `가장 인기 많은 대댓글 ${i}` });
+    data.mostSmiledComments.push({ ...commentItem, commentText: `웃음 많은 댓글 ${i}` });
+    data.feedbackComments.push({ ...commentItem, commentText: `피드백 댓글 ${i}` });
+  }
   return data;
 };
