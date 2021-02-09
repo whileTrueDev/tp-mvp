530d31a73fe7534ec8300342ee725f91ce6971e4
jiyeon <jiyeon@jiyeonui-MacBookPro.local>
2021-02-09 17:54:53 +0900

    글 삭제, 추천, 조회 핸들러 생성

--- server/src/resources/communityBoard/communityBoard.controller.ts
+++ server/src/resources/communityBoard/communityBoard.controller.ts
@@ -2,22 +2,47 @@ import {
   Controller,
   Get,
   Post,
+  Put,
+  Delete,
   Body,
   Req,
-  Put,
   Param,
+  Query,
   ParseIntPipe,
-  // Delete,
+  // ValidationPipe,
+  // Res,
 } from '@nestjs/common';
 import express from 'express';
 import { CreateCommunityPostDto } from '@truepoint/shared/dist/dto/communityBoard/createCommunityPost.dto';
 import { UpdateCommunityPostDto } from '@truepoint/shared/dist/dto/communityBoard/updateCommunityPost.dto';
+
 import { CommunityBoardService } from './communityBoard.service';
 import { CommunityPostEntity } from './entities/community-post.entity';
-
 @Controller('community')
 export class CommunityBoardController {
   constructor(private readonly communityBoardService: CommunityBoardService) {}
+
+  /**
+   * 
+   * @param platform 'afreeca' | 'twitch' 플랫폼 구분
+   * @param category 'all' | 'notice' | 'recommended' 전체글, 공지글, 추천글 조회용
+   * @param page 보여질 페이지
+   * @param take 해당 페이지에 보여지는 글의 개수
+   */
+  @Get()
+  findAllPosts(
+    @Query('platform') platform: string,
+    @Query('category') category: string,
+    @Query('page', ParseIntPipe) page: number,
+    @Query('take', ParseIntPipe) take: number,
+  ): Promise<{posts: CommunityPostEntity[], total: number}> {
+    return this.communityBoardService.findAllPosts({
+      platform,
+      category: category || 'all',
+      page: page < 1 ? 1 : page,
+      take: take < 0 ? 10 : take,
+    });
+  }
 
   @Post()
   createOnePost(
@@ -25,6 +50,16 @@ export class CommunityBoardController {
     @Body() createCommunityPostDto: CreateCommunityPostDto,
   ): Promise<CommunityPostEntity> {
     return this.communityBoardService.createOnePost(createCommunityPostDto, request.ip);
+  }
+
+  @Post('/:postId/recommend')
+  recommendPost(
+    // @Req() request: express.Request,
+    // @Res() response: express.Response,
+    @Param('postId', ParseIntPipe) postId: number,
+  ): Promise<any> {
+    // 1일 1회 추천만 가능하도록 수정해야함
+    return this.communityBoardService.recommendPost(postId);
   }
 
   @Put(':postId')
@@ -35,23 +70,18 @@ export class CommunityBoardController {
     return this.communityBoardService.updateOnePost(postId, updateCommunityBoardDto);
   }
 
+  @Get(':postId')
+  findOne(@Param('postId', ParseIntPipe) postId: number): Promise<CommunityPostEntity> {
+    return this.communityBoardService.hitAndFindOnePost(postId);
+  }
+
+  @Delete(':postId')
+  remove(@Param('postId', ParseIntPipe) postId: number): Promise<boolean> {
+    return this.communityBoardService.removeOnePost(postId);
+  }
+
   @Get()
   test(): string {
     return 'test';
   }
-
-  // @Get()
-  // findAll() {
-  //   return this.communityBoardService.findAll();
-  // }
-
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.communityBoardService.findOne(+id);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.communityBoardService.remove(+id);
-  // }
 }
--- server/src/resources/communityBoard/communityBoard.service.ts
+++ server/src/resources/communityBoard/communityBoard.service.ts
@@ -1,5 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { Repository, FindManyOptions } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
 import { CreateCommunityPostDto } from '@truepoint/shared/dist/dto/communityBoard/createCommunityPost.dto';
@@ -12,6 +12,19 @@ export class CommunityBoardService {
     @InjectRepository(CommunityPostEntity)
     private readonly communityPostRepository: Repository<CommunityPostEntity>,
   ) {}
+
+  private PLATFORM_CODE = {
+    afreeca: 0,
+    twitch: 1,
+  }
+
+  // 게시글 일반글, 공지글 구분용
+  private POST_CATEGORY_CODE = {
+    normal: 0,
+    notice: 1,
+  }
+
+  private getPlatformCode = (platform: string): number => this.PLATFORM_CODE[platform]
 
   async createOnePost(createCommunityPostDto: CreateCommunityPostDto, ip: string): Promise<CommunityPostEntity> {
     try {
@@ -31,21 +44,68 @@ export class CommunityBoardService {
     }
   }
 
-  // findAll() {
-  //   return `This action returns all communityBoard`;
-  // }
-
-  async findOnePost(postId: number): Promise<CommunityPostEntity> {
+  async findAllPosts({
+    platform, page, take, category,
+  }: {
+    platform: string,
+    page: number,
+    take: number,
+    category: string
+  }): Promise<{posts: CommunityPostEntity[], total: number}> {
+    // 리턴값은 {posts, total, }
     try {
-      const post = await this.communityPostRepository.findOne({ postId });
-      if (!post) {
-        throw new HttpException('no post with that postId', HttpStatus.BAD_REQUEST);
+      let option: FindManyOptions<CommunityPostEntity> = {
+        where: { platform: this.getPlatformCode(platform) },
+        take,
+        skip: (page - 1) * take,
+        order: { createDate: 'DESC' },
+      };
+
+      if (category === 'notice') {
+        // 공지사항만
+        option = {
+          ...option,
+          where: {
+            category: this.POST_CATEGORY_CODE[category],
+            platform: this.getPlatformCode(platform),
+          },
+        };
+      } if (category === 'recommended') {
+        // 추천글만
+        option = {
+          ...option,
+          where: {
+            platform: this.getPlatformCode(platform),
+          },
+          order: { recommend: 'DESC' },
+        };
+      } else {
+        // 일반글
+        option = {
+          ...option,
+          where: {
+            platform: this.getPlatformCode(platform),
+          },
+        };
       }
-      return post;
+
+      const [result, total] = await this.communityPostRepository.findAndCount(option);
+      return {
+        posts: result,
+        total,
+      };
     } catch (error) {
       console.error(error);
-      throw new HttpException('error in find one post', HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new HttpException('error in find all posts', HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
+
+  async findOnePost(postId: number): Promise<CommunityPostEntity> {
+    const post = await this.communityPostRepository.findOne({ postId });
+    if (!post) {
+      throw new HttpException('no post with that postId', HttpStatus.BAD_REQUEST);
+    }
+    return post;
   }
 
   async updateOnePost(
@@ -53,14 +113,54 @@ export class CommunityBoardService {
     updateCommunityPostDto: UpdateCommunityPostDto,
   ): Promise<CommunityPostEntity> {
     const post = await this.findOnePost(postId);
-    return this.communityPostRepository.save({
-      ...post,
-      ...updateCommunityPostDto,
-    });
-    return post;
+    try {
+      return this.communityPostRepository.save({
+        ...post,
+        ...updateCommunityPostDto,
+      });
+    } catch (error) {
+      console.error(error);
+      throw new HttpException('error in updateOnePost', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
-  // remove(id: number) {
-  //   return `This action removes a #${id} communityBoard`;
-  // }
+  // 개별 글 조회시 조회수+1 한 후 해당 글 리턴
+  async hitAndFindOnePost(postId: number): Promise<CommunityPostEntity> {
+    const post = await this.findOnePost(postId);
+    const hitCount = post.hit;
+    try {
+      return this.communityPostRepository.save({
+        ...post,
+        hit: hitCount + 1,
+      });
+    } catch (error) {
+      console.error(error);
+      throw new HttpException('error in hitAndFindOnePost', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async removeOnePost(postId: number): Promise<boolean> {
+    const post = await this.findOnePost(postId);
+    try {
+      await this.communityPostRepository.remove(post);
+      return true;
+    } catch (error) {
+      console.error(error);
+      throw new HttpException('error in removeOnePost', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async recommendPost(postId: number): Promise<CommunityPostEntity> {
+    const post = await this.findOnePost(postId);
+    const recommendCount = post.recommend;
+    try {
+      return await this.communityPostRepository.save({
+        ...post,
+        recommend: recommendCount + 1,
+      });
+    } catch (error) {
+      console.error(error);
+      throw new HttpException('error in recommendPost', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }
