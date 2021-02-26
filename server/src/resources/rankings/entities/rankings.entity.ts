9fb335e6a86f4b57974d41b91e81a5ff23b9f66f
chanuuuuu <qkrcksdn0208@naver.com>
2021-02-26 17:24:45 +0900

    comment 추가

--- server/src/resources/rankings/entities/rankings.entity.ts
+++ server/src/resources/rankings/entities/rankings.entity.ts
@@ -15,22 +15,22 @@ export class RankingsEntity implements Rankings {
   @Column()
   creatorName: string;
 
-  @CreateDateColumn({ type: 'timestamp' })
+  @CreateDateColumn({ type: 'timestamp', comment: '테이블에 삽입된 날짜' })
   createDate: Date;
 
-  @Column({ type: 'timestamp' })
+  @Column({ type: 'timestamp', comment: '방송이 종료된 날짜' })
   streamDate: Date;
 
-  @Column({ type: 'mediumint', default: 0 })
+  @Column({ type: 'mediumint', default: 0, comment: '시청자 수' })
   viewer: number;
 
-  @Column({ type: 'smallint', default: 0 })
+  @Column({ type: 'smallint', default: 0, comment: '웃음 점수' })
   smileScore: number;
 
-  @Column({ type: 'smallint', default: 0 })
+  @Column({ type: 'smallint', default: 0, comment: '답답함 점수' })
   frustrateScore: number;
 
-  @Column({ type: 'smallint', default: 0 })
+  @Column({ type: 'smallint', default: 0, comment: '감탄 점수' })
   admireScore: number;
 
   constructor(partial: Partial<RankingsEntity>) {
