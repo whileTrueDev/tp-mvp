9ae6c9508b688cbadd26e8df2ec3192a23ad55ee
jiyeon <jiyeon@jiyeonui-MacBookPro.local>
2021-03-02 13:18:55 +0900

    시청자반응 엔티티 생성

--- server/src/resources/userReaction/userReaction.module.ts
+++ server/src/resources/userReaction/userReaction.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserReactionService } from './userReaction.service';
 import { UserReactionController } from './userReaction.controller';
-
+import { UserReactionEntity } from './entities/userReaction.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([
+    UserReactionEntity,
+  ])],
   controllers: [UserReactionController],
   providers: [UserReactionService],
 })
