73da039c2aa218e1dfcfbb73871550ed022f354f
jiyeon <jiyeon@jiyeonui-MacBookPro.local>
2021-03-03 09:57:58 +0900

    dependency추가

--- client/web/src/organisms/mainpage/ranking/UserReaction.tsx
+++ client/web/src/organisms/mainpage/ranking/UserReaction.tsx
@@ -155,7 +155,7 @@ export default function UserReaction(): JSX.Element {
         fullWidth
       />
     </form>
-  ), [classes.form, handleSubmit]);
+  ), [classes.form, classes.formRow, handleSubmit]);
 
   return (
     <section className={classes.userReactionContainer}>
