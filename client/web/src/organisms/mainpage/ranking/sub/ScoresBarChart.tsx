06130a49f08f3b615ec61bf7b4c758621dae2b18
jiyeon <jiyeon@jiyeonui-MacBookPro.local>
2021-03-09 13:26:19 +0900

    별 표시하는 함수 분리

--- client/web/src/organisms/mainpage/ranking/sub/ScoresBarChart.tsx
+++ client/web/src/organisms/mainpage/ranking/sub/ScoresBarChart.tsx
@@ -22,6 +22,45 @@ interface PlottablePoint extends Highcharts.Point {
   plotY: number;
 }
 
+/**
+ * 막대그래프 안에 1,2,3위 별 표시
+ * @param renderer Highcharts.SVGRenderer
+ * @param order 금=0, 은=1, 동=2
+ * @param x 별이 표시될 x좌표
+ * @param y 별이 표시될 y좌표
+ */
+function createStar(renderer: Highcharts.SVGRenderer, order: number, x: number, y: number) {
+  // 별에 그라디언트 넣기 위한 색 설정
+  const starColors = [
+    { id: 'gold', startColor: '#f5f542', endColor: '#c9a234' }, // 금
+    { id: 'silver', startColor: '#d2d6d5', endColor: '#7f9090' }, // 은
+    { id: 'bronze', startColor: '#ff8800', endColor: '#9f5c02' }, // 동
+  ];
+  const { id: gradientId, startColor, endColor } = starColors[order];
+  // 그라디언트 생성
+  const gradient = renderer.createElement('linearGradient')
+    .attr({
+      id: gradientId, x1: '0%', y1: '0%', x2: '0%', y2: '100%',
+    }).add(renderer.defs);
+  renderer.createElement('stop')
+    .attr({
+      offset: '0%', style: `stop-color:${startColor}`,
+    }).add(gradient);
+  renderer.createElement('stop')
+    .attr({
+      offset: '100%', style: `stop-color:${endColor}`,
+    }).add(gradient);
+
+  // 별모양 생성
+  const star = renderer.createElement('polygon');
+  star.attr({
+    points: '20,5 25,20 40,20 30,30 35,45 20,35 5,45 10,30 0,20 15,20', // 별의 각 모서리 좌표 x,y
+    fill: `url(#${gradientId})`,
+    zIndex: 5,
+    transform: `translate(${x},${y}) scale(0.4)`,
+  }).add();
+}
+
 function ScoresBarChart({
   title, data, loading, column, barColor,
 }: ScoresBarChartProps): JSX.Element {
@@ -43,41 +82,12 @@ function ScoresBarChart({
           } = this;
           if (!series[0] || series[0].data.length === 0) return;
 
-          // 별에 그라디언트 넣기 위한 색 설정
-          const starColors = [
-            { id: 'gold', startColor: '#f5f542', endColor: '#c9a234' }, // 금
-            { id: 'silver', startColor: '#d2d6d5', endColor: '#7f9090' }, // 은
-            { id: 'bronze', startColor: '#ff8800', endColor: '#9f5c02' }, // 동
-          ];
-
-          // 1,2,3 위 금은동 표시
+          // 1,2,3위에 대해 금은동 별 표시
           for (let i = 0; i < 3; i += 1) {
             const point = series[0].data[i] as PlottablePoint;
             const x = point.plotX + 2; // 별이 표시될 x좌표
             const y = plotHeight; // 별이 표시될 y 좌표
-            const { id: gradientId, startColor, endColor } = starColors[i];
-            // 그라디언트 생성
-            const gradient = renderer.createElement('linearGradient')
-              .attr({
-                id: gradientId, x1: '0%', y1: '0%', x2: '0%', y2: '100%',
-              }).add(renderer.defs);
-            renderer.createElement('stop')
-              .attr({
-                offset: '0%', style: `stop-color:${startColor}`,
-              }).add(gradient);
-            renderer.createElement('stop')
-              .attr({
-                offset: '100%', style: `stop-color:${endColor}`,
-              }).add(gradient);
-
-            // 별모양 생성
-            const star = renderer.createElement('polygon');
-            star.attr({
-              points: '20,5 25,20 40,20 30,30 35,45 20,35 5,45 10,30 0,20 15,20', // 별의 각 모서리 좌표 x,y
-              fill: `url(#${gradientId})`,
-              zIndex: 5,
-              transform: `translate(${x},${y}) scale(0.4)`,
-            }).add();
+            createStar(renderer, i, x, y);
           }
         },
       },
@@ -98,7 +108,6 @@ function ScoresBarChart({
         color: barColor,
         pointWidth: 30,
       },
-
     },
     legend: { enabled: false },
     series: [],
