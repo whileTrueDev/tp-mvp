edc056070d73065c8f75efc12d409bf652a70724
jiyeon <jiyeon@jiyeonui-MacBookPro.local>
2021-03-19 13:50:10 +0900

    폴라차트 배경이 중복 생성되지 않도록 수정

--- client/web/src/organisms/mainpage/ranking/ViewerComparisonPolarAreaCard.tsx
+++ client/web/src/organisms/mainpage/ranking/ViewerComparisonPolarAreaCard.tsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, {
+  useEffect, useRef, useState,
+} from 'react';
 import { useTheme } from '@material-ui/core/styles';
 import purple from '@material-ui/core/colors/purple';
 import blue from '@material-ui/core/colors/blue';
@@ -17,11 +19,10 @@ import ShowSnack from '../../../atoms/snackbar/ShowSnack';
 
 import { useStyles as usePolatChartStyles } from './style/polarChartStyle';
 import {
-  getChartSize,
+  getCoordsAndPanes,
   toPolarAreaData,
   polarAreaLabelFormatter,
-  createArc,
-  createGradationBlobBackground,
+  createBackground,
   CustomPointOption,
 } from './polar/polarChartUtils';
 
@@ -131,83 +132,67 @@ function ViewerComparisonPolarAreaCard(): JSX.Element {
 
   // 데이터가 변경되었을 때 실행하는 훅
   // 1.플랫폼 별 총 시청자 수에 따라 차트 크기와 중심 좌표를 계산,
-  // 2.물방울 모양 그라데이션 배경 생성
-  // 3.차트 옆 반원모양 호 생성
-  // 4.로고 위치 조정
-  // 5.차트옵션 변경 - 시청자수에 따라 차트 크기 조정, series 데이터 추가
+  // 2.배경 생성
+  // 3.로고 위치 조정
+  // 4.차트옵션 변경 - 시청자수에 따라 차트 크기 조정, series 데이터 추가
   useEffect(() => {
-    if (!chartRef.current || !data) return;
-    const { afreeca, twitch } = data;
+    if (chartRef.current && data) {
+      const { afreeca, twitch } = data;
+      const {
+        plotWidth, renderer,
+      } = chartRef.current.chart;
 
-    const compensationPx = -30; // 차트 크기(지름) 조정값
-    const [afreecaChartSize, twitchChartSize] = getChartSize(afreeca.total, twitch.total, compensationPx);
-    const {
-      plotWidth, plotHeight, renderer, plotLeft, plotTop,
-    } = chartRef.current.chart;
-    const verticalCenter = plotHeight * 0.5; // 차트 y좌표값
-    const horizontalCenter = plotWidth * 0.5; // 차트 x좌표 기본값
+      const {
+        afreecaChartCoord,
+        twitchChartCoord,
+        afreecaPaneOptions,
+        twitchPaneOptions,
+      } = getCoordsAndPanes(afreeca.total, twitch.total, chartRef.current.chart);
 
-    const supplementDistance = compensationPx * 0.2; // 차트 크기(지름) 조정값에 따라 차트 중심 x좌표값을 조절한다
-    const afreecaHorizontalCenter = horizontalCenter - afreecaChartSize / 2 + supplementDistance;
-    const twitchHorizontalCenter = horizontalCenter + twitchChartSize / 2 - supplementDistance;
-    // 아프리카 차트 x,y좌표와 반지름r
-    const afreecaChartCoord = {
-      x: afreecaHorizontalCenter + plotLeft,
-      y: verticalCenter + plotTop,
-      r: afreecaChartSize / 2,
-    };
-    // 트위치 차트 x,y좌표와 반지름r
-    const twitchChartCoord = {
-      x: twitchHorizontalCenter + plotLeft,
-      y: verticalCenter + plotTop,
-      r: twitchChartSize / 2,
-    };
+      // 배경(물방울모양, 그래프 옆 호) 그리기---------------------------------------------------
+      const {
+        container: bgContainer,
+        afreecaArc,
+        twitchArc,
+      } = createBackground(renderer, [afreecaChartCoord, twitchChartCoord]);
 
-    // 물방울모양 그라데이션 배경 그리기---------------------------------------------------
-    createGradationBlobBackground(renderer, [afreecaChartCoord, twitchChartCoord]);
+      // 로고 위치 조정---------------------------------------------------
+      const { x: afreecaArcX } = afreecaArc.getBBox();
+      const { x: twitchArcX, width: twitchArcWidth } = twitchArc.getBBox();
+      const distanceFromArc = 30; // arc에서 얼마나 떨어질것인지 px단위
 
-    // 호 그리기---------------------------------------------------
-    const afreecaArc = createArc(renderer, afreecaChartCoord, 'left', blue[400]);
-    afreecaArc.add();
-    const twitchArc = createArc(renderer, twitchChartCoord, 'right', purple[400]);
-    twitchArc.add();
+      if (afreecaLogoRef.current) {
+        afreecaLogoRef.current.style.setProperty('right', `${(plotWidth - afreecaArcX) + distanceFromArc}px`);
+      }
+      if (twitchLogoRef.current) {
+        twitchLogoRef.current.style.setProperty('left', `${(twitchArcX + twitchArcWidth) + distanceFromArc}px`);
+      }
 
-    // 로고 위치 조정---------------------------------------------------
-    const { x: afreecaArcX } = afreecaArc.getBBox();
-    const { x: twitchArcX, width: twitchArcWidth } = twitchArc.getBBox();
-    const distanceFromArc = 30; // arc에서 얼마나 떨어질것인지 px단위
+      // 차트옵션 변경 - 시청자수에 따라 차트 크기 조정, series 데이터 추가
+      setOptions({
+        pane: [afreecaPaneOptions, twitchPaneOptions] as Highcharts.PaneOptions, // pane 타입정의가 배열을 못받게 되어있어서 임시로 타입 단언 사용함
+        series: [{
+          type: 'column',
+          name: 'afreeca',
+          yAxis: 0,
+          xAxis: 0,
+          data: toPolarAreaData(afreeca.data, blue),
+        }, {
+          type: 'column',
+          name: 'twitch',
+          yAxis: 1,
+          xAxis: 1,
+          data: toPolarAreaData(twitch.data, purple),
+        }],
+      });
 
-    if (afreecaLogoRef.current) {
-      afreecaLogoRef.current.style.setProperty('right', `${(plotWidth - afreecaArcX) + distanceFromArc}px`);
+      return () => {
+        bgContainer.destroy(); // 데이터변경시 생성되었던 배경을 지운다
+      };
     }
-    if (twitchLogoRef.current) {
-      twitchLogoRef.current.style.setProperty('left', `${(twitchArcX + twitchArcWidth) + distanceFromArc}px`);
-    }
-
-    // 차트옵션 변경 - 시청자수에 따라 차트 크기 조정, series 데이터 추가
-    setOptions({
-      pane: [{
-        size: afreecaChartSize,
-        center: [`${afreecaHorizontalCenter}`, `${verticalCenter}`],
-      }, {
-        size: twitchChartSize,
-        center: [`${twitchHorizontalCenter}`, `${verticalCenter}`],
-      }] as Highcharts.PaneOptions, // pane 타입정의가 배열을 못받게 되어있어서 임시로 타입 단언 사용함
-      series: [{
-        type: 'column',
-        name: 'afreeca',
-        yAxis: 0,
-        xAxis: 0,
-        data: toPolarAreaData(afreeca.data, blue),
-      }, {
-        type: 'column',
-        name: 'twitch',
-        data: toPolarAreaData(twitch.data, purple),
-        yAxis: 1,
-        xAxis: 1,
-      }],
-    });
-  }, [data]);
+    return undefined;
+  },
+  [data]);
 
   return (
     <section className={classes.polarAreaContainer}>
