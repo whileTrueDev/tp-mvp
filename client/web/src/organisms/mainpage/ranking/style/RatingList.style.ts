3989990ab4fa0a924cfb25cc1e009aa98ec9acc8
Jiyeon <a1919361@gmail.com>
2021-04-12 14:47:58 +0900

    평점별 목록 글자 색 수정

--- client/web/src/organisms/mainpage/ranking/style/RatingList.style.ts
+++ client/web/src/organisms/mainpage/ranking/style/RatingList.style.ts
@@ -1,48 +1,56 @@
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { lighten } from '@material-ui/core/styles/colorManipulator';
 // listItem 컴포넌트
-export const useRatingsListItemStyles = makeStyles((theme: Theme) => createStyles({
-  listItem: {
-    height: theme.spacing(12),
-    border: '2px solid black',
-    '&:not(:last-child)': {
-      marginBottom: theme.spacing(2),
+export const useRatingsListItemStyles = makeStyles((theme: Theme) => {
+  const contrastColor = theme.palette.common.black;
+
+  return createStyles({
+    listItem: {
+      height: theme.spacing(12),
+      border: '2px solid black',
+      '&:not(:last-child)': {
+        marginBottom: theme.spacing(2),
+      },
     },
-  },
-  avatarImage: {
-    marginRight: theme.spacing(2),
-    border: `${theme.spacing(0.5)}px solid ${theme.palette.text.primary}`,
-    width: theme.spacing(7),
-    height: theme.spacing(7),
-  },
-  creatorName: {
-    marginRight: theme.spacing(1),
-    fontSize: theme.typography.body1.fontSize,
-    fontWeight: theme.typography.fontWeightBold,
-  },
-  ratingText: {
-    textAlign: 'center',
-    fontSize: theme.typography.body1.fontSize,
-  },
-  orderText: {
-    padding: theme.spacing(0, 1),
-    backgroundColor: 'blue',
-    color: theme.palette.common.white,
-    fontWeight: theme.typography.fontWeightBold,
-  },
-  [`listItem-${1}`]: {
-    backgroundColor: lighten(theme.palette.primary.main, 0.25),
-  },
-  [`listItem-${2}`]: {
-    backgroundColor: lighten(theme.palette.primary.main, 0.5),
-  },
-  [`listItem-${3}`]: {
-    backgroundColor: lighten(theme.palette.primary.main, 0.75),
-  },
-  [`listItem-${4}`]: {
-    backgroundColor: lighten(theme.palette.primary.main, 0.9),
-  },
-}));
+    avatarImage: {
+      marginRight: theme.spacing(2),
+      border: `${theme.spacing(0.5)}px solid ${theme.palette.text.primary}`,
+      width: theme.spacing(7),
+      height: theme.spacing(7),
+    },
+    creatorName: {
+      marginRight: theme.spacing(1),
+      fontSize: theme.typography.body1.fontSize,
+      fontWeight: theme.typography.fontWeightBold,
+    },
+    ratingText: {
+      textAlign: 'center',
+      fontSize: theme.typography.body1.fontSize,
+    },
+    orderText: {
+      padding: theme.spacing(0, 1),
+      backgroundColor: 'blue',
+      color: theme.palette.common.white,
+      fontWeight: theme.typography.fontWeightBold,
+    },
+    [`listItem-${1}`]: {
+      backgroundColor: lighten(theme.palette.primary.main, 0.25),
+      color: contrastColor,
+    },
+    [`listItem-${2}`]: {
+      backgroundColor: lighten(theme.palette.primary.main, 0.5),
+      color: contrastColor,
+    },
+    [`listItem-${3}`]: {
+      backgroundColor: lighten(theme.palette.primary.main, 0.75),
+      color: contrastColor,
+    },
+    [`listItem-${4}`]: {
+      backgroundColor: lighten(theme.palette.primary.main, 0.9),
+      color: contrastColor,
+    },
+  });
+});
 
 // list 컴포넌트
 export const useRatingsListStyles = makeStyles((theme: Theme) => createStyles({
