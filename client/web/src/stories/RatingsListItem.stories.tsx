7e4c15f06ba8031fb77df49d819797c51f2b0852
Jiyeon <a1919361@gmail.com>
2021-04-22 17:10:00 +0900

    주간평점순위 목록 컴포넌트 수정 - 변동순위 추가, 플랫폼 이미지 추가, 배경색 추가

--- client/web/src/stories/RatingsListItem.stories.tsx
+++ client/web/src/stories/RatingsListItem.stories.tsx
@@ -12,9 +12,9 @@ const Template: Story<RatingsListItemProps> = (args) => <RatingsListItem {...arg
 export const Default = Template.bind({});
 Default.args = {
   creatorId: 'bht0205',
-  averageRating: 10,
-  ratingCount: 3,
+  averageRating: 3,
   nickname: 'AZ.형태형',
+  rankChange: 1,
   logo: 'https://profile.img.afreecatv.com/LOGO/bh/bht0205/bht0205.jpg',
   platform: 'afreeca',
 };
