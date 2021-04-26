f78a74d6742a648eea4ad9a626e2c26cac30dc81
hwasurr <iamsupermazinga@gmail.com>
2021-04-26 17:58:52 +0900

    FIx: 색상 변경

--- server/src/middleware/colorizedMorgan.ts
+++ server/src/middleware/colorizedMorgan.ts
@@ -4,6 +4,7 @@ import chalk from 'chalk'; // or you can use the require('chalk') syntax too
 const colorizedMorgan = morgan((tokens, req, res) => {
   const status = tokens.status(req, res);
   let statusText = chalk.bold(status);
+  if (status.startsWith('2')) statusText = chalk.green.bold(status);
   if (status.startsWith('4')) statusText = chalk.hex('#ffb142').bold(status);
   if (status.startsWith('5')) statusText = chalk.bgRed.bold(status);
 
@@ -15,7 +16,7 @@ const colorizedMorgan = morgan((tokens, req, res) => {
     chalk.yellow.bold(tokens.res(req, res, 'content-length'), 'byte ->'),
     chalk.hex('#2ed573').bold(`${tokens['response-time'](req, res)} ms`),
     process.env.NODE_ENV === 'production' ? chalk.hex('#adf573').bold(tokens['remote-addr'](req, res)) : undefined,
-    process.env.NODE_ENV === 'production' ? chalk.hex('#e1d573').bold(`[${tokens.date(req, res).toLocaleString()}]`) : undefined,
+    chalk.hex('#e1d573').bold(`[${tokens.date(req, res).toLocaleString()}]`),
   ].join(' ');
 });
 
