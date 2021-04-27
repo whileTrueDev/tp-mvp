3a6042de7513300c1924e829b9e8840792c80703
Jiyeon <a1919361@gmail.com>
2021-04-27 14:16:31 +0900

    린트 오류 해결

--- client/web/src/organisms/mainpage/ranking/style/CreatorComment.style.ts
+++ client/web/src/organisms/mainpage/ranking/style/CreatorComment.style.ts
@@ -112,7 +112,6 @@ export const useCreatorCommentItemStyle = makeStyles((theme: Theme) => createSty
   },
 }));
 
-
 export const useCreatorCommentListStyle = makeStyles((theme: Theme) => createStyles({
   commentsContainer: {
 
