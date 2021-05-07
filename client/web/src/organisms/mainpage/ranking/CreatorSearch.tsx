02e53261adb3df24bfa081a4b5e612a9ea8a88e0
Jiyeon <a1919361@gmail.com>
2021-05-07 11:31:09 +0900

    방송인 검색 테이블 추가

--- client/web/src/organisms/mainpage/ranking/CreatorSearch.tsx
+++ client/web/src/organisms/mainpage/ranking/CreatorSearch.tsx
@@ -1,20 +1,29 @@
 import useAxios from 'axios-hooks';
 import React from 'react';
 import { User } from '@truepoint/shared/dist/interfaces/User.interface';
+import { useTheme } from '@material-ui/core/styles';
 import CreatorSearchTable from './search/CreatorSearchTable';
 
-export interface CreatorSearchProps {
-
-}
-
-export default function CreatorSearch(props: CreatorSearchProps): JSX.Element {
-  const [{ data, loading, error }, refetch] = useAxios<User[]>('/users/id-list');
+export default function CreatorSearch(): JSX.Element {
+  const [{ data, loading }] = useAxios<User[]>('/users/id-list');
+  const theme = useTheme();
 
   return (
-    <div>
-      <CreatorSearchTable
-        data={data}
-      />
+    <div style={{
+      width: '100%',
+      background: theme.palette.background.paper,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: theme.spacing(4),
+    }}
+    >
+      <div style={{ maxWidth: '1800px', width: '100%' }}>
+        <CreatorSearchTable
+          data={data}
+          loading={loading}
+        />
+      </div>
 
     </div>
   );
--- client/web/src/organisms/mainpage/ranking/search/CreatorSearchTable.tsx
+++ client/web/src/organisms/mainpage/ranking/search/CreatorSearchTable.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { User } from '@truepoint/shared/dist/interfaces/User.interface';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Avatar, Chip } from '@material-ui/core';
 import { useHistory } from 'react-router';
+import { useTheme } from '@material-ui/core/styles';
 import MaterialTable from '../../../../atoms/Table/MaterialTable';
 
 export interface CreatorSearchTableProps {
   data: User[] | undefined;
+  loading: boolean;
 }
 
 export default function CreatorSearchTable(props: CreatorSearchTableProps): JSX.Element {
-  const { data } = props;
+  const { data, loading } = props;
+  const theme = useTheme();
   const history = useHistory();
-  // event?: React.MouseEvent<Element, MouseEvent> | undefined, rowData?: User | undefined
   const onRowClick = (event: React.MouseEvent<Element, MouseEvent> | undefined, rowData: User | undefined) => {
-    console.log(event, rowData);
     const platform = rowData?.afreeca ? 'afreeca' : 'twitch';
     const creatorId = rowData?.afreeca ? rowData?.afreeca.afreecaId : rowData?.twitch?.twitchId;
     history.push(`/ranking/${platform}/${creatorId}`);
@@ -34,35 +36,63 @@ export default function CreatorSearchTable(props: CreatorSearchTableProps): JSX.
           ),
         },
         {
-          width: '20%',
+          width: '5%',
           align: 'center',
           title: ' ',
           render: (rowData) => <Avatar src={rowData.twitch?.logo || rowData.afreeca?.logo} />,
         },
         {
-          width: '20%',
+          width: '30%',
           align: 'center',
           title: '활동명',
           field: 'nickName',
         },
         {
-          width: '55%',
+          width: '30%',
           align: 'center',
           title: '카테고리',
           render: (rowData) => (
             <div>
               {rowData.twitch?.categories && rowData.twitch?.categories.map(category => (
-                <Chip key={category.categoryId} label={category.name}/>
+                <Chip
+                style={{fontSize: theme.typography.body1.fontSize}}
+                key={category.categoryId} label={category.name}/>
               ))}
               {rowData.afreeca?.categories && rowData.afreeca?.categories.map(category => (
-                <Chip key={category.categoryId} label={category.name}/>
+                <Chip
+                style={{fontSize: theme.typography.body1.fontSize}}
+                key={category.categoryId} label={category.name}/>
               ))}
             </div>
           ),
         },
+        // {
+        //   width: '30%',
+        //   align: 'center',
+        //   title: '평점',
+        // },
       ]}
       data={data || []}
       onRowClick={onRowClick}
+      isLoading={loading}
+      title="방송인 검색"
+      options={{
+        pageSize: 20,
+        pageSizeOptions: [20, 40],
+        searchFieldAlignment: 'left',
+        headerStyle: {
+          backgroundColor: theme.palette.primary.main,
+          color: theme.palette.primary.contrastText,
+          fontSize: theme.typography.h6.fontSize,
+        },
+        searchFieldVariant: 'outlined',
+        searchFieldStyle: {
+          fontSize: theme.typography.h6.fontSize,
+        },
+        rowStyle: {
+          fontSize: theme.typography.h6.fontSize,
+        },
+      }}
     />
   );
 }
