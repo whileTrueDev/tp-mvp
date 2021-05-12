5348ed0ec180b383a8e60b8a9f9d3381791d1bce
Jiyeon <a1919361@gmail.com>
2021-05-12 16:24:36 +0900

    감정점수 10점 이상인경우 그래프상 10의 위치에 고정

--- client/web/src/organisms/mainpage/ranking/topten/ScoreBar.tsx
+++ client/web/src/organisms/mainpage/ranking/topten/ScoreBar.tsx
@@ -3,30 +3,34 @@ import React, { useMemo } from 'react';
 import { useProgressBar, useTopTenList } from '../style/TopTenList.style';
 
 const MIN = 0;
-const MAX = 10;
-/**
+interface Props{
+  score: number;
+  max?: number;
+}
+function ScoreBar(props: Props): JSX.Element {
+  const { score, max = 10 } = props;
+  const progressBarStyles = useProgressBar();
+  const classes = useTopTenList();
+  /**
  * 0 ~ 100 사이의 숫자를 MIN ~ MAX사이의 숫자로 변환하는 함수
  * 점수는 0~10의 값으로 들어오고, 
  * LinearProgress에서는 0~100사이의 값이 필요하므로 변환 필요
  * @param value 0~100 사이의 숫자
  * @returns MIN~MAX 사이로 변환된 값
  */
-const normalize = (value: number): number => Number((((value - MIN) * 100) / (MAX - MIN) || 0).toFixed(2));
-
-function ScoreBar({ score }: {score: number}): JSX.Element {
-  const progressBarStyles = useProgressBar();
-  const classes = useTopTenList();
-  const normalizedScore = useMemo(() => normalize(score), [score]);
+  const normalizedScore = useMemo(() => (
+    Number((((score - MIN) * 100) / (max - MIN) || 0).toFixed(2))
+  ), [max, score]);
   return (
     <div>
       <LinearProgress
         variant="determinate"
         aria-valuemin={0}
-        aria-valuemax={10}
+        aria-valuemax={max}
         aria-valuenow={score}
-        valueBuffer={10}
+        valueBuffer={max}
         classes={progressBarStyles}
-        value={normalizedScore}
+        value={Math.min(100, normalizedScore)}
       />
       {score && (
       <Typography
@@ -36,7 +40,7 @@ function ScoreBar({ score }: {score: number}): JSX.Element {
           transform: `translateX(${(10 - score) * (-10)}%`,
         }}
       >
-        {score ? `${score.toFixed(2)}` : 0}
+        {score ? `${Math.min(10, Number(score.toFixed(2)))}` : 0}
       </Typography>
       )}
 
