7b0aa64bdd11059dfff7e5a992fcd99636394bd7
Jiyeon <a1919361@gmail.com>
2021-06-15 18:30:20 +0900

    notfound page 움직이는 이미지 삭제

--- client/web/src/pages/others/PageNotFound.tsx
+++ client/web/src/pages/others/PageNotFound.tsx
@@ -5,6 +5,7 @@ import { useHistory, Link } from 'react-router-dom';
 import Appbar from '../../organisms/shared/Appbar';
 import Footer from '../../organisms/shared/footer/Footer';
 import styles from '../../organisms/mainpage/main/style/ProductHero.style';
+import TruepointLogo from '../../atoms/TruepointLogo';
 
 export default function PageNotFound(): JSX.Element {
   const classes = styles();
@@ -24,40 +25,37 @@ export default function PageNotFound(): JSX.Element {
   }
 
   return (
-    <div>
+    <Grid container direction="column" justify="space-between" style={{ height: '100vh' }}>
       <Appbar />
-      <section>
-        <div className={classes.root}>
-          <Container>
-            <Grid container direction="row" justify="center" alignItems="center">
-              <Grid item md={9} sm={12} xs={12} className={classes.wraper}>
-                <div className={classes.main}>
-                  <h1 className={classes.mainTitle}>존재하지 않는 페이지입니다</h1>
-                </div>
-                <div className={classes.main}>
-                  <p className={classes.mainContent}>{`${second}초 후 메인페이지로 이동합니다`}</p>
-                  <p className={classes.mainContent}>아래 버튼을 누르면 바로 메인페이지로 이동합니다</p>
-                </div>
-                <div className={classes.mainExcept}>
-                  <Button
-                    className={classes.button}
-                    component={Link}
-                    to="/"
-                  >
-                    홈페이지로 이동
-                  </Button>
-                  <div className={classes.buttonLine} />
-                </div>
-              </Grid>
-              <Grid item md={3} className={classes.imgWraper}>
-                <img src="/images/main/heromain.svg" alt="HeroMain" className={classes.mainSVGEffect} />
-                <img src="/images/main/herosub.svg" alt="HeroSub" className={classes.subSVGEffect} />
-              </Grid>
-            </Grid>
-          </Container>
-        </div>
-      </section>
+      <Container className={classes.root}>
+        <Grid container direction="column" justify="center" alignItems="center">
+          <Grid item>
+            <TruepointLogo />
+          </Grid>
+          <Grid item>
+            <div className={classes.main}>
+              <h1 className={classes.mainTitle}>존재하지 않는 페이지입니다</h1>
+            </div>
+          </Grid>
+          <Grid item>
+            <div className={classes.main}>
+              <p className={classes.mainContent}>{`${second}초 후 메인페이지로 이동합니다`}</p>
+              <p className={classes.mainContent}>아래 버튼을 누르면 바로 메인페이지로 이동합니다</p>
+            </div>
+          </Grid>
+          <Grid item>
+            <Button
+              variant="outlined"
+              component={Link}
+              to="/"
+            >
+              홈페이지로 이동
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
       <Footer />
-    </div>
+    </Grid>
+
   );
 }
