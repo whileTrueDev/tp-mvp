01d4415d419768d2a0ed6bea2ae05695eb8a80d6
Jiyeon <a1919361@gmail.com>
2021-06-15 16:44:24 +0900

    카테고리별 1위 컴포넌트 생성및 캐러셀에 추가

--- client/web/src/organisms/mainpage/ranking/RankingMain.tsx
+++ client/web/src/organisms/mainpage/ranking/RankingMain.tsx
@@ -13,6 +13,7 @@ import WeeklyLineCard from './WeeklyLineCard';
 import useMediaSize from '../../../utils/hooks/useMediaSize';
 import SearchGuideBannerSlide from './sub/SearchGuideBannerSlide';
 import PageTitle from '../shared/PageTitle';
+import FirstPlaceCreators from './sub/FirstPlaceCreators';
 
 export function CarouselSection(): JSX.Element {
   const carousel = useCarouselStyle();
@@ -28,6 +29,7 @@ export function CarouselSection(): JSX.Element {
           autoPlay={false}
           navButtonsProps={{ style: { backgroundColor: 'transparent', transform: 'translateY(-2rem)' }, className: 'carousel-button' }}
         >
+          <FirstPlaceCreators />
           <SearchGuideBannerSlide />
           <ViewerComparisonPolarAreaCard />
           <WeeklyLineCard />
