4316dcb4f7a4bf6469d6fb05ed5ea1df4ac74e05
Jiyeon <a1919361@gmail.com>
2021-06-16 17:49:37 +0900

    [fix] 인증번호 발송 메일주소 변경(env 값 변경)

--- server/src/resources/auth/emailVerification.service.ts
+++ server/src/resources/auth/emailVerification.service.ts
@@ -65,7 +65,6 @@ export class EmailVerificationService {
       // 2. 메일로 코드 전송
       await this.mailerService.sendMail({
         to: email, // list of receivers
-        // from: 'noreply-----truepointceo@gmail.com', // sender address
         subject: '트루포인트 회원가입 인증 코드', // Subject line
         html: `
         <h1>
@@ -113,7 +112,6 @@ export class EmailVerificationService {
     try {
       await this.mailerService.sendMail({
         to: email, // list of receivers
-        // from: 'noreply-----truepointceo@gmail.com', // sender address
         subject: '트루포인트 임시 비밀번호 발급', // Subject line
         html: `
         <h1>
