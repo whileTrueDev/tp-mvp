81a4337dfe4b7bbaf13f00c13301cbeb7e2a3928
Jiyeon <a1919361@gmail.com>
2021-06-17 18:14:47 +0900

    [Fix] 회원정보 화면 아바타 부분 스타일 수정

--- client/web/src/organisms/mypage/userInfo/UserSetting.tsx
+++ client/web/src/organisms/mypage/userInfo/UserSetting.tsx
@@ -1,25 +1,36 @@
 import {
-  Paper, CircularProgress, Avatar, Typography,
+  Paper, CircularProgress, Avatar, Typography, Button,
 } from '@material-ui/core';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { User } from '@truepoint/shared/dist/interfaces/User.interface';
 import useAxios from 'axios-hooks';
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import useAuthContext from '../../../utils/hooks/useAuthContext';
 import { useStyles } from '../dashboard/UserProfile';
 import useMediaSize from '../../../utils/hooks/useMediaSize';
 
 const useUserSettingStyle = makeStyles((theme: Theme) => createStyles({
-  avatar: {
+  smallAvatar: {
     width: theme.spacing(6),
     height: theme.spacing(6),
-    marginRight: theme.spacing(3),
+  },
+  avatarContainer: {
+    marginRight: theme.spacing(1),
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  editButton: {
+    position: 'absolute',
+    right: theme.spacing(2),
+    top: theme.spacing(2),
   },
 }));
 
 export default function UserSetting(): JSX.Element {
   const classes = useStyles();
-  const { avatar } = useUserSettingStyle();
+  const { smallAvatar, avatarContainer, editButton } = useUserSettingStyle();
   const auth = useAuthContext();
   const [profileRequestObject, getProfile] = useAxios<User>({
     url: 'users', method: 'GET', params: { userId: auth.user.userId },
@@ -32,6 +43,26 @@ export default function UserSetting(): JSX.Element {
     }
   }, [auth.user.userId, getProfile]);
 
+  const InformationDisplay = useMemo(() => profileRequestObject.data && (
+    <div>
+      {/* 이름 */}
+      <div className={classes.flexBox}>
+        <Typography component="span">닉네임 :&nbsp;</Typography>
+        <Typography variant="h6" component="span" className={classes.bold}>
+          {`${profileRequestObject.data.nickName}`}
+        </Typography>
+        <Typography component="span">&nbsp;님</Typography>
+      </div>
+
+      <div>
+        {/* 이메일 */}
+        {profileRequestObject.data.mail && (
+        <Typography>{`이메일 : ${profileRequestObject.data.mail}`}</Typography>
+        )}
+      </div>
+    </div>
+  ), [classes.bold, classes.flexBox, profileRequestObject.data]);
+
   return (
     <Paper className={classes.container}>
       {/* 로딩중 */}
@@ -43,24 +74,26 @@ export default function UserSetting(): JSX.Element {
       && profileRequestObject.data
       && (
       <>
-        <Avatar className={isMobile ? avatar : classes.avatar} src={profileRequestObject.data.profileImage || ''} />
-
-        <div>
+        <div className={avatarContainer}>
+          <Avatar className={isMobile ? smallAvatar : classes.avatar} src={profileRequestObject.data.profileImage || ''} />
           {/* 이름 */}
-          <div className={classes.flexBox}>
-            <Typography variant="h4" className={classes.bold}>
-              {`${profileRequestObject.data.nickName || profileRequestObject.data.userId}`}
-              <Typography className={classes.bold} component="span" variant="h6">&nbsp;님</Typography>
-            </Typography>
-          </div>
-
-          <div>
-            {/* 이메일 */}
-            {profileRequestObject.data.mail && (
-              <Typography>{profileRequestObject.data.mail}</Typography>
-            )}
-          </div>
+          {profileRequestObject.data.name && (
+          <Typography variant={isMobile ? 'h6' : 'h4'} className={classes.bold}>
+            {`${profileRequestObject.data.name}`}
+          </Typography>
+          )}
         </div>
+
+        {InformationDisplay}
+
+        <Button
+          variant="contained"
+          color="primary"
+          size={isMobile ? 'small' : 'medium'}
+          className={editButton}
+        >
+          정보 수정
+        </Button>
       </>
       )}
     </Paper>
