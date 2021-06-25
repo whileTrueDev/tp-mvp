111eff2ea23e171ede06e2556036568274357a49
Jiyeon <a1919361@gmail.com>
2021-06-25 14:09:17 +0900

    [Fix] 링크 주소 수정

--- server/src/admin-panel/components/community-post-link.tsx
+++ server/src/admin-panel/components/community-post-link.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { PLATFORM_CODE } from './community-post-platform-label';
 import getFrontHost from '../../utils/getFrontHost';
+
+// 아프리카=0, 트위치=1, 자유게시판=2 플랫폼 구분용 컬럼
+const PLATFORM_NAME = {
+  0: 'afreeca',
+  1: 'twitch',
+  2: 'free',
+};
 
 type Props = Record<string, any>
 
 const CommunityPostLink = ({ record }: Props): JSX.Element => {
   const { params } = record;
   const { platform, postId } = params;
-  const href = `${getFrontHost()}/community-board/${PLATFORM_CODE[platform]}/view/${postId}`;
+
+  const href = `${getFrontHost()}/community-board/${PLATFORM_NAME[platform]}/view/${postId}`;
 
   return (
     <div style={{ marginBottom: '24px' }}>
