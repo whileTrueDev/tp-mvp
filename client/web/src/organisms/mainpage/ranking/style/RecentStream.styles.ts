42c204b926093cabbbff917d1f5ef4d2fcd9b219
Jiyeon <a1919361@gmail.com>
2021-06-29 16:52:36 +0900

    [Style] 최근방송목록 데스크톱 스타일 수정(제플린에 있는대로 수정)

--- client/web/src/organisms/mainpage/ranking/style/RecentStream.styles.ts
+++ client/web/src/organisms/mainpage/ranking/style/RecentStream.styles.ts
@@ -1,94 +1,109 @@
 import { makeStyles } from '@material-ui/core';
 
 export const sectionHeight = 320;
+const itemBoxHeight = 260;
 
-const useRecentStreamStyles = makeStyles((theme) => ({
-  section: {
-    backgroundColor: theme.palette.background.paper,
-    height: sectionHeight,
-    position: 'relative',
-    borderRadius: theme.spacing(0.5),
-    overflow: 'hidden',
-  },
-  itembox: {
-    height: sectionHeight,
-    [theme.breakpoints.down('sm')]: {
-      marginLeft: theme.spacing(1),
+const useRecentStreamStyles = makeStyles((theme) => {
+  const bgColor = theme.palette.type === 'light'
+    ? theme.palette.grey[200]
+    : theme.palette.background.default;
+  return ({
+    section: {
+      backgroundColor: theme.palette.background.paper,
+      height: sectionHeight,
+      position: 'relative',
+      borderRadius: theme.spacing(0.5),
+      overflow: 'hidden',
     },
-  },
-  leftDecorator: {
-    position: 'absolute',
-    left: -200,
-    top: 0,
-    height: sectionHeight,
-  },
-  title: {
-    textOverflow: 'clip',
-    overflow: 'hidden',
-    whiteSpace: 'nowrap',
-    textDecoration: 'none',
-    fontWeight: 'bold',
-    color: theme.palette.text.secondary,
-    transition: theme.transitions.create(
-      'color', { easing: theme.transitions.easing.sharp, duration: theme.transitions.duration.short },
-    ),
-    '&:hover': {
-      cursor: 'pointer',
-      color: theme.palette.text.primary,
+    streamListSection: {
+      height: sectionHeight,
+      width: '42%',
+      position: 'absolute',
+      right: theme.spacing(2),
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
     },
-  },
-  subtitle: {
-    marginLeft: theme.spacing(2),
-    fontWeight: 'bold',
-  },
-  row: {
-    color: theme.palette.text.secondary,
-    transition: theme.transitions.create(
-      'color', { easing: theme.transitions.easing.sharp, duration: theme.transitions.duration.short },
-    ),
-    '&:hover': {
-      cursor: 'pointer',
-      color: theme.palette.text.primary,
-    },
-    '&>*': {
-      marginRight: theme.spacing(1),
-    },
-    marginTop: theme.spacing(3.5),
-    [theme.breakpoints.down('sm')]: {
-      backgroundColor: theme.palette.divider,
-      marginBottom: theme.spacing(0.25),
-      marginTop: theme.spacing(0),
-      padding: theme.spacing(0.5, 1),
-      '&>*': {
-        marginRight: 0,
+    itembox: {
+      height: itemBoxHeight,
+      backgroundColor: bgColor,
+      overflowY: 'auto',
+      [theme.breakpoints.down('sm')]: {
+        height: 'auto',
+        backgroundColor: 'transparent',
       },
     },
-  },
-  titleText: {
-    textOverflow: 'clip',
-    overflow: 'hidden',
-    whiteSpace: 'nowrap',
-    textDecoration: 'none',
-    fontWeight: 'bold',
-    [theme.breakpoints.down('sm')]: {
-      fontSize: theme.spacing(1.5),
+    title: {
+      textOverflow: 'clip',
+      overflow: 'hidden',
+      whiteSpace: 'nowrap',
+      textDecoration: 'none',
+      fontWeight: 'bold',
+      color: theme.palette.text.secondary,
+      transition: theme.transitions.create(
+        'color', { easing: theme.transitions.easing.sharp, duration: theme.transitions.duration.short },
+      ),
+      '&:hover': {
+        cursor: 'pointer',
+        color: theme.palette.text.primary,
+      },
     },
-  },
-  subText: {
-    color: theme.palette.text.secondary,
-    [theme.breakpoints.down('sm')]: {
-      fontSize: theme.spacing(1.25),
+    subtitle: {
+      marginLeft: theme.spacing(2),
+      fontWeight: 'bold',
     },
-  },
-  profileImage: {
-    position: 'absolute',
-    right: 0,
-    top: 0,
-    height: sectionHeight,
-  },
-  tooltip: {
-    maxWidtH: 600,
-  },
-}));
+    row: {
+      padding: theme.spacing(1, 2),
+      color: theme.palette.text.secondary,
+      transition: theme.transitions.create(
+        'color', { easing: theme.transitions.easing.sharp, duration: theme.transitions.duration.short },
+      ),
+      '&:hover': {
+        cursor: 'pointer',
+        backgroundColor: theme.palette.divider,
+      },
+      [theme.breakpoints.down('sm')]: {
+        backgroundColor: bgColor,
+        marginBottom: theme.spacing(0.25),
+        padding: theme.spacing(0.5, 1),
+        '&>*': {
+          marginRight: 0,
+        },
+      },
+    },
+    titleText: {
+      textOverflow: 'clip',
+      overflow: 'hidden',
+      whiteSpace: 'nowrap',
+      textDecoration: 'none',
+      fontWeight: 'bold',
+      [theme.breakpoints.down('xs')]: {
+        fontSize: theme.spacing(1.5),
+      },
+      [theme.breakpoints.down('sm')]: {
+        fontSize: theme.spacing(1.75),
+      },
+    },
+    subText: {
+      color: theme.palette.text.secondary,
+      fontSize: theme.spacing(1.75),
+      [theme.breakpoints.down('xs')]: {
+        fontSize: theme.spacing(1.25),
+      },
+      [theme.breakpoints.down('sm')]: {
+        fontSize: theme.spacing(1.5),
+      },
+    },
+    profileImage: {
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      height: sectionHeight,
+    },
+    tooltip: {
+      maxWidtH: 600,
+    },
+  });
+});
 
 export default useRecentStreamStyles;
