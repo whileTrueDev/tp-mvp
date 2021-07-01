f8a417e4ed01436582d443d19275fe4cfbae5463
chanuuuuu <dn0208@gmail.com>
2021-07-01 10:00:05 +0900

    [Refactor] day.js 포맷팅 함수 주석 추가

--- client/web/src/utils/dateExpression.tsx
+++ client/web/src/utils/dateExpression.tsx
@@ -11,6 +11,17 @@ interface makeDate {
   finishAt?: any;
 }
 
+/**
+ * day.js를 통한 날짜 포맷팅 함수
+ * @param date
+ * 포맷팅할 날짜 데이터 (string, Date 객체 등),   
+ * - 날짜에 대한 정의가 없다면 빈 Day.js 객체가 반환된다.  
+ * @param format
+ * 포맷 형식이나 사전 정의된 포맷 이름으로, 포맷에 대한 정의가 없다면 Day.js 객체가 반환된다.  
+ * 사용 가능한 포맷명은 아래와 같다.
+ * - 'default' : 'YYYY-MM-DD HH:mm:ss'  
+ * - 'date-only' : 'YYYY-MM-DD'
+ */
 export function dayjsFormatter(date?: string | Date | null | undefined, format?: 'default' | 'date-only' | string): any {
   if (date) {
     switch (format) {
