88e63a957fd9f6caa5eaa737c1935283a2fd0d9f
Jiyeon <a1919361@gmail.com>
2021-07-20 14:11:47 +0900

    차트 데이터, 포매터 설정

--- client/web/src/organisms/mainpage/ranking/creatorInfo/ScoresHistorySection.tsx
+++ client/web/src/organisms/mainpage/ranking/creatorInfo/ScoresHistorySection.tsx
@@ -21,6 +21,40 @@ Highcharts.setOptions({
   },
 });
 
+function formatNumber(type: string, number: number): string {
+  switch (type) {
+    case '최고 시청자 수':
+      return Highcharts.numberFormat(number, 0, undefined, ',');
+    default:
+      return Highcharts.numberFormat(number, 2, undefined, ',');
+  }
+}
+
+function tooltipFormatter(this: Highcharts.TooltipFormatterContextObject) {
+  const {
+    x, y, series, point,
+  } = this;
+  const value = point.options ? point.options.custom?.originValue : y;
+  const number = formatNumber(series.name, Number(value));
+  return `
+  <b>${dayjs(x).format('ll')}</b>
+  <table>
+  <tr>
+    <td>${point.options.title || ''}</td>
+  </tr>
+  <tr>
+    <td style="color: ${series.options.color}">${series.name}: </td>
+    <td><b>${number}</b></td>
+  </tr>
+  </table>
+`;
+}
+
+function dataLabelFormatter(this: Highcharts.PointLabelObject) {
+  const number = Number(this.point.options?.custom?.originValue) || 0;
+  return formatNumber(this.series.name, number);
+}
+
 type ScoresHistoryControlButton = {
   key: keyof ScoreHistoryData,
   label: string,
@@ -40,8 +74,31 @@ const ScoresHistorySectionHeight = 280;
 export default function ScoresHistorySection({ creatorId }: {creatorId: string}): JSX.Element {
   const theme = useTheme();
   const { isMobile: isDownSm, isDownXs } = useMediaSize();
+  const [{ data, loading }] = useAxios<ScoreHistoryData[]>({
+    url: '/rankings/scores/history',
+    method: 'get',
+    params: {
+      creatorId,
+    },
+  });
+
+  const [selectedButton, setSelectedButton] = useState<ScoresHistoryControlButton>(buttons[0]);
+  const [buttonState, setButtonState] = useState<keyof ScoreHistoryData>('viewer');
+
   // 차트컨테이너 ref
   const chartRef = useRef<{chart: Highcharts.Chart, container: React.RefObject<HTMLDivElement>}>(null);
+
+  useEffect(() => {
+    if (chartRef.current) {
+      const { chart } = chartRef.current;
+      if (loading) {
+        chart.showLoading();
+      } else {
+        chart.hideLoading();
+      }
+    }
+  }, [loading]);
+
   const [chartOptions, setChartOptions] = useState<Highcharts.Options>({
     chart: {
       height: ScoresHistorySectionHeight,
@@ -51,69 +108,75 @@ export default function ScoresHistorySection({ creatorId }: {creatorId: string})
     credits: { enabled: false },
     xAxis: {
       type: 'datetime',
+      crosshair: true,
       labels: {
-        format: '{value:%y-%m-%d}',
+        format: '{value:%m-%d}',
       },
     },
     yAxis: {
       title: { text: undefined },
     },
-    legend: {
-      enabled: false,
-    },
-    plotOptions: {
-      series: {
-        connectNulls: true,
-      },
+    legend: { enabled: false },
+    tooltip: {
+      useHTML: true,
+      formatter: tooltipFormatter,
     },
   });
-  const [{ data }] = useAxios<ScoreHistoryData[]>({
-    url: '/rankings/scores/history',
-    method: 'get',
-    params: {
-      creatorId,
-    },
-  });
-
-  const [buttonState, setButtonState] = useState<keyof ScoreHistoryData>('viewer');
 
   useEffect(() => {
     if (!data) return;
+
     // 데이터 중 값이 null이 아닌 첫번째 인덱스 찾기
     const firstNotNullItemIndex = data.findIndex((item) => item[buttonState]);
     const validData = data.slice(firstNotNullItemIndex);
 
-    let series = validData.map((item) => [dayjs(item.date).valueOf(), item[buttonState]]);
+    const tempData = validData.map((d) => {
+      const originValue = d[buttonState];
+      let y: number|null;
+      let overMax = false;
+
+      if (buttonState !== 'viewer' && originValue && originValue > 10) {
+        y = 10;
+        overMax = true;
+      } else {
+        y = originValue === null ? originValue : Number(originValue);
+      }
+      return {
+        x: dayjs(d.date).valueOf(),
+        y,
+        title: d.title || undefined,
+        marker: {
+          enabled: overMax,
+          symbol: 'circle',
+          radius: 6,
+          lineColor: 'white',
+          lineWidth: 4,
+        },
+        dataLabels: {
+          enabled: true,
+          formatter: dataLabelFormatter,
+        },
+        custom: { overMax, originValue },
+      };
+    });
+
     const minDate = validData[0].date || undefined;
     const maxDate = validData[validData.length - 1].date || undefined;
-    if (firstNotNullItemIndex === -1) {
-      series = [];
-    }
-    let max: number | null = null;
-    if (!['viewer', 'rating'].includes(buttonState)) {
-      max = 10;
-    }
-    const min = buttonState === 'rating' ? null : 0;
+    const xAxis = {
+      min: dayjs(minDate).valueOf(),
+      max: dayjs(maxDate).valueOf(),
+    };
+    const yAxis = {
+      max: !['viewer'].includes(buttonState) ? 10 : null,
+      min: 0,
+    };
+
     setChartOptions({
       chart: {
         backgroundColor: theme.palette.background.paper,
       },
-      series: [
-        {
-          type: 'line',
-          data: series,
-        },
-      ],
-      xAxis: {
-        min: dayjs(minDate).valueOf(),
-        max: dayjs(maxDate).valueOf(),
-      },
-      yAxis: {
-        min,
-        max,
-      },
       lang: {
-        noData: '매겨진 별점이 없습니다. 별점을 매겨보세요!',
+        noData: buttonState === 'rating' ? '매겨진 별점이 없습니다. 별점을 매겨보세요!' : '데이터가 없습니다',
       },
       noData: {
         position: {
@@ -121,13 +184,24 @@ export default function ScoresHistorySection({ creatorId }: {creatorId: string})
           verticalAlign: 'middle',
         },
       },
+      series: [
+        {
+          type: 'line',
+          data: firstNotNullItemIndex === -1 ? [] : tempData,
+          name: selectedButton.label,
+        },
+      ],
+      xAxis,
+      yAxis,
       plotOptions: {
         series: {
+          connectNulls: true,
           color: theme.palette.primary.dark,
+          marker: { enabled: false },
         },
       },
     });
-  }, [buttonState, data, theme.palette.background.paper, theme.palette.primary.dark]);
+  }, [buttonState, data, selectedButton.label, theme.palette.background.paper, theme.palette.primary.dark]);
 
   return (
     <Grid container style={{ backgroundColor: theme.palette.background.paper }}>
@@ -146,7 +220,10 @@ export default function ScoresHistorySection({ creatorId }: {creatorId: string})
           buttons.map((button) => (
             <Button
               key={button.key}
-              onClick={() => setButtonState(button.key)}
+              onClick={() => {
+                setButtonState(button.key);
+                setSelectedButton(button);
+              }}
               startIcon={isDownXs ? undefined : button.icon}
               variant="outlined"
               size={isDownSm ? 'small' : 'medium'}
