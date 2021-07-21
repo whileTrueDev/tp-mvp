c7a5086e8d495263870a54ac8a81fb41c2a82b7b
Jiyeon <a1919361@gmail.com>
2021-07-21 10:13:45 +0900

    버튼 툴팁 추가

--- client/web/src/organisms/mainpage/ranking/creatorInfo/ScoresHistorySection.tsx
+++ client/web/src/organisms/mainpage/ranking/creatorInfo/ScoresHistorySection.tsx
@@ -1,12 +1,12 @@
 import HighchartsReact from 'highcharts-react-official';
-import React, { useRef } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { useTheme } from '@material-ui/core/styles';
-import { Button, Grid } from '@material-ui/core';
+import { Button, Grid, Tooltip } from '@material-ui/core';
 import useMediaSize from '../../../../utils/hooks/useMediaSize';
 import useScoresHistoryChartOptions,
 { highCharts } from '../../../../utils/hooks/useScoresHistoryChartOptions';
 import CenterLoading from '../../../../atoms/Loading/CenterLoading';
-import { buttons, useScoresHistoryButton } from '../../../../store/scoresHistoryButton';
+import { buttons, ScoresHistoryControlButton, useScoresHistoryButton } from '../../../../store/scoresHistoryButton';
 
 export default function ScoresHistorySection({ creatorId }: {creatorId: string}): JSX.Element {
   const theme = useTheme();
@@ -21,12 +21,54 @@ export default function ScoresHistorySection({ creatorId }: {creatorId: string})
     container: React.RefObject<HTMLDivElement>
   }>(null);
 
+  const getButtonStyle = useCallback((button: ScoresHistoryControlButton) => ({
+    backgroundColor: selectedButton.key === button.key
+      ? theme.palette.primary.main : 'transparent',
+    color: selectedButton.key === button.key
+      ? theme.palette.primary.contrastText : theme.palette.text.primary,
+  }), [selectedButton.key, theme.palette.primary.contrastText, theme.palette.primary.main, theme.palette.text.primary]);
+
+  const renderButton = useCallback((button: ScoresHistoryControlButton) => {
+    const buttonStyle = getButtonStyle(button);
+    // xs이하 사이즈일때 - 아이콘만 표시, 툴팁 추가
+    if (isDownXs) {
+      return (
+        <Tooltip key={button.key} title={button.label} arrow>
+          <Button
+            onClick={() => changeButton(button)}
+            variant="outlined"
+            size="small"
+            style={{
+              ...buttonStyle,
+              width: 48,
+            }}
+          >
+            {button.icon}
+          </Button>
+        </Tooltip>
+      );
+    }
+    return (
+      <Button
+        key={button.key}
+        onClick={() => changeButton(button)}
+        startIcon={button.icon}
+        variant="outlined"
+        style={{
+          ...buttonStyle,
+          width: 'auto',
+        }}
+      >
+        {button.label}
+      </Button>
+    );
+  }, [changeButton, getButtonStyle, isDownXs]);
+
   return (
     <Grid container style={{ backgroundColor: theme.palette.background.paper }}>
       <Grid
         item
         xs={12}
-        sm={12}
         md={2}
         container
         direction={isDownSm ? 'row' : 'column'}
@@ -34,28 +76,14 @@ export default function ScoresHistorySection({ creatorId }: {creatorId: string})
         wrap="nowrap"
         style={{ padding: 8 }}
       >
-        {
-          buttons.map((button) => (
-            <Button
-              key={button.key}
-              onClick={() => changeButton(button)}
-              startIcon={isDownXs ? undefined : button.icon}
-              variant="outlined"
-              size={isDownSm ? 'small' : 'medium'}
-              style={{
-                backgroundColor: selectedButton.key === button.key
-                  ? theme.palette.primary.main : 'transparent',
-                color: selectedButton.key === button.key
-                  ? theme.palette.primary.contrastText : theme.palette.text.primary,
-                width: isDownXs ? 48 : 'auto',
-              }}
-            >
-              {isDownXs ? button.icon : button.label}
-            </Button>
-          ))
-        }
+        { buttons.map(renderButton) }
       </Grid>
-      <Grid item xs={12} sm={12} md={10} style={{ position: 'relative' }}>
+      <Grid
+        item
+        xs={12}
+        md={10}
+        style={{ position: 'relative' }}
+      >
         <HighchartsReact
           ref={chartRef}
           highcharts={highCharts}
