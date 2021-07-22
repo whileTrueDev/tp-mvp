0baef157e57835327ebd0d3550263220da6b266d
Jiyeon <a1919361@gmail.com>
2021-07-22 09:11:02 +0900

    [Add] 사용가능한 닉네임인지 확인하는 함수 작성

--- client/web/src/utils/checkAvailableNickname.ts
+++ client/web/src/utils/checkAvailableNickname.ts
@@ -1,10 +1,18 @@
-const nicknameBlackList = [
+const nicknameBlackist = [
   '관리자',
   'truepoint',
 ];
 
+export const UNAVAILABLE_NICKNAME_ERROR_MESSAGE = '사용할 수 없는 닉네임입니다. 다른 닉네임을 입력해주세요.';
+
 export function isAvailableNickname(nick: string): boolean {
   const trimmedLowercase = nick.replace(/ /g, '').toLowerCase();
-  const includedInBlackList = nicknameBlackList.includes(trimmedLowercase);
+  const includedInBlackList = nicknameBlackist.includes(trimmedLowercase);
   return !includedInBlackList;
+}
+
+export function throwErrorIfNicknameOnBlacklist(nick: string): never | void {
+  if (!isAvailableNickname(nick)) {
+    throw new Error(UNAVAILABLE_NICKNAME_ERROR_MESSAGE);
+  }
 }
