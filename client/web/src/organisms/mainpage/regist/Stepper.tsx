970606eed1222b16afde0ae16421a1260d4fe6e9
Jiyeon <a1919361@gmail.com>
2021-08-09 14:10:39 +0900

    회원가입 시 약관동의 화면부터 보이도록 원상복구

--- client/web/src/organisms/mainpage/regist/Stepper.tsx
+++ client/web/src/organisms/mainpage/regist/Stepper.tsx
@@ -20,7 +20,7 @@ function RegistStepper(): JSX.Element {
   const history = useHistory();
   const classes = useStyles();
   const { enqueueSnackbar } = useSnackbar();
-  const [activeStep, setStep] = useState(1);
+  const [activeStep, setStep] = useState(0);
   const [marketingAgreement, setAgreement] = useState(false);
   const [state, dispatch] = useReducer(myReducer, initialState);
   const [certificationInfo] = useState({}); // 휴대폰 인증 후 받은 인증유저정보
